3e68320cbb27e6e3e5da3d6dc2412ecd47e5571d
Laura Shireman <laura.shireman@certara.com>
2021-11-15 08:38:05 -0800

    Discovered that enzyme abundance data in gut is sometimes (always?) broken up into small intestine and colon rather than just 'gut'. Accounted for that with extractEnzAbund function.

--- R/extractEnzAbund.R
+++ R/extractEnzAbund.R
@@ -77,106 +77,258 @@ extractEnzAbund <- function(sim_data_file,
                                sheet = SheetToExtract,
                                col_names = FALSE))
 
+      # Extracting aggregate data ---------------------------------------------
       if("aggregate" %in% returnAggregateOrIndiv){
-            # mean data
-            StartRow_mean <- which(sim_data_xl$...1 == "Population Statistics") + 1
-            sim_data_mean <- sim_data_xl[StartRow_mean:(StartRow_mean+3), ] %>%
-                  t() %>%
-                  as.data.frame() %>% slice(-(1:3)) %>%
-                  mutate_all(as.numeric) %>%
-                  rename(Time = "V1", mean = "V2", per5 = "V3", per95 = "V4") %>%
-                  pivot_longer(names_to = "Trial", values_to = "Abundance", cols = -Time) %>%
-                  mutate(Enzyme = Enzyme)
 
-            # Checking for inhibitor
-            EffectorPresent <- str_detect(sim_data_xl$...1[StartRow_mean + 4],
-                                           "Inh")
-            if(EffectorPresent){
-                  sim_data_mean_inhib <- sim_data_xl[c(StartRow_mean, (StartRow_mean+4):(StartRow_mean+6)), ] %>%
+            # If the tissue was gut, sometimes there are separate data sets for
+            # small intestine and colon. Checking for that.
+            GutParts <- c("colon", "small intestine")[
+                  c(any(str_detect(tolower(sim_data_xl$...1), "\\(colon\\)")),
+                    any(str_detect(tolower(sim_data_xl$...1), "\\(si\\)")))]
+
+            if(length(GutParts) > 0 & tissue == "gut"){
+
+                  sim_data_mean <- list()
+
+                  for(i in GutParts){
+
+                        # mean data
+                        TimeRow <- which(sim_data_xl$...1 == "Population Statistics") + 1
+                        StartRow_mean <- which(
+                              str_detect(tolower(sim_data_xl$...1),
+                                         switch(i,
+                                                "colon" = "\\(colon\\)",
+                                                "small intestine" = "\\(si\\)")))
+                        StartRow_mean <- StartRow_mean[which(StartRow_mean > TimeRow)][1]
+
+                        sim_data_mean[[i]] <- sim_data_xl[c(TimeRow, StartRow_mean:(StartRow_mean+2)), ] %>%
+                              t() %>%
+                              as.data.frame() %>% slice(-(1:3)) %>%
+                              mutate_all(as.numeric) %>%
+                              rename(Time = "V1", mean = "V2", per5 = "V3", per95 = "V4") %>%
+                              pivot_longer(names_to = "Trial", values_to = "Abundance", cols = -Time) %>%
+                              mutate(Enzyme = enzyme)
+
+                        # Checking for inhibitor
+                        EffectorPresent <- str_detect(sim_data_xl$...1[StartRow_mean + 3],
+                                                      "Inh")
+                        if(EffectorPresent){
+                              sim_data_mean_inhib <- sim_data_xl[c(TimeRow,
+                                                                   (StartRow_mean+3):(StartRow_mean+5)), ] %>%
+                                    t() %>%
+                                    as.data.frame() %>% slice(-(1:3)) %>%
+                                    mutate_all(as.numeric) %>%
+                                    rename(Time = "V1", mean = "V2", per5 = "V3", per95 = "V4") %>%
+                                    pivot_longer(names_to = "Trial", values_to = "Abundance", cols = -Time) %>%
+                                    mutate(Enzyme = enzyme,
+                                           EffectorPresent = TRUE)
+
+                              sim_data_mean[[i]] <- bind_rows(sim_data_mean[[i]], sim_data_mean_inhib)
+                        }
+
+                        sim_data_mean[[i]]$GutPart <- i
+
+                        rm(sim_data_mean_inhib, TimeRow, StartRow_mean)
+                  }
+
+                  sim_data_mean <- bind_rows(sim_data_mean)
+
+            } else {
+
+                  # mean data
+                  StartRow_mean <- which(sim_data_xl$...1 == "Population Statistics") + 1
+                  sim_data_mean <- sim_data_xl[StartRow_mean:(StartRow_mean+3), ] %>%
                         t() %>%
                         as.data.frame() %>% slice(-(1:3)) %>%
                         mutate_all(as.numeric) %>%
                         rename(Time = "V1", mean = "V2", per5 = "V3", per95 = "V4") %>%
                         pivot_longer(names_to = "Trial", values_to = "Abundance", cols = -Time) %>%
-                        mutate(Enzyme = Enzyme,
-                               EffectorPresent = TRUE)
+                        mutate(Enzyme = enzyme)
 
-                  sim_data_mean <- bind_rows(sim_data_mean, sim_data_mean_inhib)
+                  # Checking for inhibitor
+                  EffectorPresent <- str_detect(sim_data_xl$...1[StartRow_mean + 4],
+                                                "Inh")
+                  if(EffectorPresent){
+                        sim_data_mean_inhib <- sim_data_xl[c(StartRow_mean, (StartRow_mean+4):(StartRow_mean+6)), ] %>%
+                              t() %>%
+                              as.data.frame() %>% slice(-(1:3)) %>%
+                              mutate_all(as.numeric) %>%
+                              rename(Time = "V1", mean = "V2", per5 = "V3", per95 = "V4") %>%
+                              pivot_longer(names_to = "Trial", values_to = "Abundance", cols = -Time) %>%
+                              mutate(Enzyme = enzyme,
+                                     EffectorPresent = TRUE)
 
+                        sim_data_mean <- bind_rows(sim_data_mean, sim_data_mean_inhib)
+
+                  }
             }
-
       }
 
+      # Extracting individual data --------------------------------------------
       if("individual" %in% returnAggregateOrIndiv){
 
-            # individual data
-            RowsToUse <- which(str_detect(sim_data_xl$...1,
-                                          paste("Enzyme value",
-                                                switch(tissue,
-                                                       "liver" = ".Liver.",
-                                                       "gut" = ".Gut.",
-                                                       "kidney" = ".Kidney."))) &
-                                     !str_detect(sim_data_xl$...1, "mean"))
-            RowsToUse <- c(RowsToUse[1]-1, RowsToUse)
+            # If the tissue was gut, sometimes there are separate data sets for
+            # small intestine and colon. Checking for that.
+            GutParts <- c("colon", "small intestine")[
+                  c(any(str_detect(tolower(sim_data_xl$...1), "\\(colon\\)")),
+                    any(str_detect(tolower(sim_data_xl$...1), "\\(si\\)")))]
 
-            sim_data_ind <- sim_data_xl[RowsToUse, ] %>%
-                  t() %>%
-                  as.data.frame() %>% slice(-(1:3)) %>%
-                  mutate_all(as.numeric) %>%
-                  rename(Time = "V1")
+            if(length(GutParts) > 0 & tissue == "gut"){
 
-            SubjTrial <- sim_data_xl[RowsToUse[2:length(RowsToUse)], 2:3] %>%
-                  rename(Individual = ...2, Trial = ...3) %>%
-                  mutate(SubjTrial = paste0("ID", Individual, "_", Trial))
+                  sim_data_ind <- list()
 
-            names(sim_data_ind)[2:ncol(sim_data_ind)] <- SubjTrial$SubjTrial
+                  for(i in GutParts){
 
-            sim_data_ind <- sim_data_ind %>%
-                  pivot_longer(names_to = "SubjTrial", values_to = "Abundance",
-                               cols = -Time) %>%
-                  mutate(Enzyme = Enzyme,
-                         SubjTrial = sub("ID", "", SubjTrial)) %>%
-                  separate(SubjTrial, into = c("Individual", "Trial"),
-                           sep = "_") %>%
-                  mutate(across(.cols = c("Individual", "Trial"),
-                                .fns = as.numeric))
+                        TimeRow <- which(sim_data_xl$...1 == "Individual Statistics") + 1
+                        TimeRow <- ifelse(sim_data_xl$...1[TimeRow] == "Type",
+                                          TimeRow + 1, TimeRow)
 
-            # Checking for inhibitor
-            EffectorPresent <- str_detect(sim_data_xl$...1[max(RowsToUse)+1], "Inh")
+                        RowsToUse <- intersect(
+                              which(str_detect(tolower(sim_data_xl$...1),
+                                               switch(i,
+                                                      "colon" = "\\(colon\\)",
+                                                      "small intestine" = "\\(si\\)"))),
+                              which(!str_detect(tolower(sim_data_xl$...1),
+                                                "with inh")) )
+                        RowsToUse <- RowsToUse[which(RowsToUse > TimeRow)]
 
-            RowsToUse_inhib <- c(RowsToUse[1],
-                                 which(str_detect(sim_data_xl$...1, "Enzyme value with Inh") &
-                                       !str_detect(sim_data_xl$...1, "mean|percentile")))
+                        sim_data_ind[[i]] <- sim_data_xl[c(TimeRow, RowsToUse), ] %>%
+                              t() %>%
+                              as.data.frame() %>% slice(-(1:3)) %>%
+                              mutate_all(as.numeric) %>%
+                              rename(Time = "V1")
 
-            if(EffectorPresent){
-                  sim_data_ind_inhib <-
-                        sim_data_xl[RowsToUse_inhib, ] %>%
+                        SubjTrial <- sim_data_xl[RowsToUse, 2:3] %>%
+                              rename(Individual = ...2, Trial = ...3) %>%
+                              mutate(SubjTrial = paste0("ID", Individual, "_", Trial))
+
+                        names(sim_data_ind[[i]])[2:ncol(sim_data_ind[[i]])] <- SubjTrial$SubjTrial
+
+                        sim_data_ind[[i]] <- sim_data_ind[[i]] %>%
+                              pivot_longer(names_to = "SubjTrial", values_to = "Abundance",
+                                           cols = -Time) %>%
+                              mutate(Enzyme = enzyme,
+                                     SubjTrial = sub("ID", "", SubjTrial)) %>%
+                              separate(SubjTrial, into = c("Individual", "Trial"),
+                                       sep = "_") %>%
+                              mutate(across(.cols = c("Individual", "Trial"),
+                                            .fns = as.numeric))
+
+                        # Checking for inhibitor
+                        EffectorPresent <- str_detect(sim_data_xl$...1[max(RowsToUse)+1], "Inh")
+
+                        if(EffectorPresent){
+                              RowsToUse_inhib <- which(
+                                    str_detect(tolower(sim_data_xl$...1),
+                                               switch(i,
+                                                      "colon" = "with inh \\(colon\\)",
+                                                      "small intestine" = "with inh \\(si\\)")))
+                              RowsToUse_inhib <- RowsToUse_inhib[which(RowsToUse_inhib > TimeRow)]
+
+                              sim_data_ind_inhib <-
+                                    sim_data_xl[c(TimeRow, RowsToUse_inhib), ] %>%
+                                    t() %>%
+                                    as.data.frame() %>% slice(-(1:3)) %>%
+                                    mutate_all(as.numeric) %>%
+                                    rename(Time = "V1")
+
+                              names(sim_data_ind_inhib)[
+                                    2:ncol(sim_data_ind_inhib)] <- SubjTrial$SubjTrial
+
+                              sim_data_ind_inhib <- sim_data_ind_inhib %>%
+                                    pivot_longer(names_to = "SubjTrial", values_to = "Abundance",
+                                                 cols = -Time) %>%
+                                    mutate(Enzyme = enzyme, EffectorPresent = TRUE,
+                                           SubjTrial = sub("ID", "", SubjTrial)) %>%
+                                    separate(SubjTrial, into = c("Individual", "Trial"),
+                                             sep = "_") %>%
+                                    mutate(across(.cols = c("Individual", "Trial"),
+                                                  .fns = as.numeric))
+
+                              sim_data_ind[[i]] <- bind_rows(sim_data_ind[[i]], sim_data_ind_inhib)
+
+                              rm(RowsToUse_inhib, sim_data_ind_inhib)
+
+                        }
+
+                        rm(RowsToUse, TimeRow)
+
+                        sim_data_ind[[i]]$GutPart <- i
+                  }
+
+                  sim_data_ind <- bind_rows(sim_data_ind)
+
+            } else {
+
+                  # individual data
+                  RowsToUse <- which(str_detect(sim_data_xl$...1,
+                                                paste("Enzyme value",
+                                                      switch(tissue,
+                                                             "liver" = ".Liver.",
+                                                             "gut" = ".Gut.",
+                                                             "kidney" = ".Kidney."))) &
+                                           !str_detect(sim_data_xl$...1, "mean"))
+
+                  RowsToUse <- c(RowsToUse[1]-1, RowsToUse)
+
+                  sim_data_ind <- sim_data_xl[RowsToUse, ] %>%
                         t() %>%
                         as.data.frame() %>% slice(-(1:3)) %>%
                         mutate_all(as.numeric) %>%
                         rename(Time = "V1")
 
-                  names(sim_data_ind_inhib)[
-                        2:ncol(sim_data_ind_inhib)] <- SubjTrial$SubjTrial
+                  SubjTrial <- sim_data_xl[RowsToUse[2:length(RowsToUse)], 2:3] %>%
+                        rename(Individual = ...2, Trial = ...3) %>%
+                        mutate(SubjTrial = paste0("ID", Individual, "_", Trial))
 
-                  sim_data_ind_inhib <- sim_data_ind_inhib %>%
+                  names(sim_data_ind)[2:ncol(sim_data_ind)] <- SubjTrial$SubjTrial
+
+                  sim_data_ind <- sim_data_ind %>%
                         pivot_longer(names_to = "SubjTrial", values_to = "Abundance",
                                      cols = -Time) %>%
-                        mutate(Enzyme = Enzyme, EffectorPresent = TRUE,
+                        mutate(Enzyme = enzyme,
                                SubjTrial = sub("ID", "", SubjTrial)) %>%
                         separate(SubjTrial, into = c("Individual", "Trial"),
                                  sep = "_") %>%
                         mutate(across(.cols = c("Individual", "Trial"),
                                       .fns = as.numeric))
 
-                  sim_data_ind <- bind_rows(sim_data_ind, sim_data_ind_inhib)
+                  # Checking for inhibitor
+                  EffectorPresent <- str_detect(sim_data_xl$...1[max(RowsToUse)+1], "Inh")
 
-                  rm(RowsToUse_inhib)
+                  RowsToUse_inhib <- c(RowsToUse[1],
+                                       which(str_detect(sim_data_xl$...1, "Enzyme value with Inh") &
+                                                   !str_detect(sim_data_xl$...1, "mean|percentile")))
 
+                  if(EffectorPresent){
+                        sim_data_ind_inhib <-
+                              sim_data_xl[RowsToUse_inhib, ] %>%
+                              t() %>%
+                              as.data.frame() %>% slice(-(1:3)) %>%
+                              mutate_all(as.numeric) %>%
+                              rename(Time = "V1")
+
+                        names(sim_data_ind_inhib)[
+                              2:ncol(sim_data_ind_inhib)] <- SubjTrial$SubjTrial
+
+                        sim_data_ind_inhib <- sim_data_ind_inhib %>%
+                              pivot_longer(names_to = "SubjTrial", values_to = "Abundance",
+                                           cols = -Time) %>%
+                              mutate(Enzyme = enzyme, EffectorPresent = TRUE,
+                                     SubjTrial = sub("ID", "", SubjTrial)) %>%
+                              separate(SubjTrial, into = c("Individual", "Trial"),
+                                       sep = "_") %>%
+                              mutate(across(.cols = c("Individual", "Trial"),
+                                            .fns = as.numeric))
+
+                        sim_data_ind <- bind_rows(sim_data_ind, sim_data_ind_inhib)
+
+                        rm(RowsToUse_inhib)
+
+                  }
+
+                  rm(RowsToUse)
             }
-
-            rm(RowsToUse)
-
       }
 
       TimeUnits <- sim_data_xl$...1[which(str_detect(sim_data_xl$...1, "^Time"))][1]
@@ -206,12 +358,17 @@ extractEnzAbund <- function(sim_data_file,
       if("EffectorPresent" %in% names(Data)){
             Data <- Data %>%
                   mutate(EffectorPresent = ifelse(is.na(EffectorPresent),
-                                                      FALSE, EffectorPresent))
+                                                  FALSE, EffectorPresent))
+      }
+
+      if(length(GutParts) > 0 & tissue == "gut"){
+            Data <- Data %>% mutate(Tissue = GutPart)
+      } else {
+            Data$Tissue <- tissue
       }
 
       Data <- Data %>%
-            mutate(Time_units = tolower({{TimeUnits}}),
-                   Tissue = tissue) %>%
+            mutate(Time_units = tolower({{TimeUnits}})) %>%
             arrange(across(any_of(c("Enzyme", "Tissue", "EffectorPresent",
                                     "Individual", "Trial", "Time")))) %>%
             select(any_of(c("Enzyme", "Tissue", "EffectorPresent",
