6fc03e312073dabb510e91b74c9895570b89ee6b
Laura Shireman <laura.shireman@certara.com>
2022-03-01 07:00:36 -0800

    ct_plot_overlay: Playing with some options for graphing. UNDER CONSTRUCTION

--- R/ct_plot_overlay.R
+++ R/ct_plot_overlay.R
@@ -5,21 +5,18 @@
 #' concentration-time data from multiple Simcyp Simulator output files for easy
 #' comparisons. UNDER CONSTRUCTION.
 #'
-#' @param sim_obs_dataframe the data.frame with multiple sets of concentration-time
-#'   data, including observed data where appropriate
-#' @param aggregate_option What type of aggregate measure should be shown?
-#'   Options are any combination of "mean" (arithmetic mean), "geomean"
-#'   (geometric mean), "median", "per5" (5th percentile), "per95" (95th
-#'   percentile), "per10" (10th percentile), or "per90" (90th percentile).
-#'   Default is "geomean". Example of useage: \code{aggregate_option = c("mean",
-#'   "per5", "per95")}
-#' @param colorBy What column in \code{sim_obs_dataframe} should be used for coloring
-#'   the lines and/or points on the graph? This should be unquoted, e.g.,
-#'   \code{colorBy = Tissue}.
+#' @param sim_obs_dataframe the data.frame with multiple sets of
+#'   concentration-time data, including observed data where appropriate
+#' @param mean_type show "geometric" (default) or "arithmetic" means for the
+#'   main (thickest) line for each data set. If this summary stat is not
+#'   available in the simulator output, it will not be plotted.
+#' @param colorBy What column in \code{sim_obs_dataframe} should be used for
+#'   coloring the lines and/or points on the graph? This should be unquoted,
+#'   e.g., \code{colorBy = Tissue}.
 #' @param facet_column1 If you would like to break up your graph into small
 #'   multiples, you can break the graphs up by up to two columns in
-#'   \code{sim_obs_dataframe}. What should be the 1st column to break up the data by?
-#'   This should be unquoted.
+#'   \code{sim_obs_dataframe}. What should be the 1st column to break up the
+#'   data by? This should be unquoted.
 #' @param facet_column2 What should be the 2nd column to break up the data into
 #'   small multiples by? This should be unquoted.
 #'
@@ -31,7 +28,7 @@
 #' # ct_plot_overlay(sim_obs_dataframe = CT, facet_column1 = Compound,
 #' #                 facet_column2 = Tissue)
 #'
-#'
+#' 
 ct_plot_overlay <- function(sim_obs_dataframe,
                             aggregate_option = "geomean",
                             colorBy = File,
@@ -48,17 +45,30 @@ ct_plot_overlay <- function(sim_obs_dataframe,
         mutate(Group = paste(File, Trial, Tissue, CompoundID, Compound, Inhibitor))
     
     obs_data <- sim_obs_dataframe %>% filter(Simulated == FALSE)
-    sim_obs_dataframe <- sim_obs_dataframe %>%
-        filter(Trial %in% aggregate_option & Simulated == TRUE)
+    sim_dataframe <- sim_obs_dataframe %>%
+        filter(Simulated == TRUE)
     
-    ggplot(sim_obs_dataframe,
-           aes(x = Time, y = Conc, color = !!colorBy, linetype = !!linetypeBy,
+    print("This graph contains the following unique combinations of data (make sure they are what you were expecting):")
+    print(sim_obs_dataframe %>% filter(Trial == switch(mean_type, 
+                                                       "arithmetic" = "mean",
+                                                       "geometric" = "geomean")) %>% 
+              select(File, Tissue, CompoundID, Compound, Inhibitor) %>% unique())
+    
+    ggplot(sim_dataframe %>% filter(Trial == switch(mean_type, 
+                                                    "arithmetic" = "mean",
+                                                    "geometric" = "geomean")),
+           # aes(x = Time, y = Conc, color = !!colorBy, linetype = !!linetypeBy, # Comment this for easier code development
+           aes(x = Time, y = Conc, color = colorBy, linetype = linetypeBy, # Uncomment this for easier code development
                group = Group)) +
         geom_line() +
         geom_point(data = obs_data) +
-        facet_grid(rows = vars(!!facet_column1),
-                   cols = vars(!!facet_column2),
+        # facet_grid(rows = vars(!!facet_column1), # Comment this for easier code development
+        #            cols = vars(!!facet_column2), # Comment this for easier code development
+        facet_grid(rows = vars(facet_column1), # Uncomment this for easier code development
+                   cols = vars(facet_column2), # Uncomment this for easier code development
                    scales = "free") +
+        scale_linetype_manual(values = rep("solid", 4)) +
+        # scale_x_continuous(limits = c(336-24, 336)) +
         theme(panel.background = element_rect(fill = "white", color = NA),
               panel.border = element_rect(color = "black", fill = NA),
               strip.background = element_rect(fill = "white"),
@@ -72,12 +82,6 @@ ct_plot_overlay <- function(sim_obs_dataframe,
     
 }
 
-# ct_plot_overlay(sim_obs_dataframe = CT, facet_column1 = Compound,
-#                 facet_column2 = Tissue)
-#
-# ct_plot_overlay(sim_obs_dataframe = CT, aggregate_option = c("mean", "per5", "per95"),
-#                 facet_column1 = Compound,
-#                 facet_column2 = Tissue, linetype = Trial)
 
 
 
