e64728030323474cfeaeabd722931eb1c82a859b
Laura Shireman <laura.shireman@certara.com>
2022-04-11 11:53:40 -0700

    ct_plot_gui: Added note clarifying that this does not currently work.

--- R/ct_plot_gui.R
+++ R/ct_plot_gui.R
@@ -1,6 +1,7 @@
 #' Run the ct_plot function through a graphical user interface
 #'
-#' Opens the shiny app version of the function \code{\link{ct_plot}}.
+#' Opens the shiny app version of the function \code{\link{ct_plot}}. CURRENTLY
+#' UNDER CONSTRUCTION AND NOT WORKING.
 #'
 #' @return
 #' @export
@@ -9,7 +10,7 @@
 #' # You don't need to specify any arguments to this function
 #' # in the console. Just type the following:
 #' shiny_ct_plot()
-#'
+#' 
 ct_plot_gui <- function(){
     
     appDir <- system.file("shiny", "ct_plot_app", package = "SimcypConsultancy")
