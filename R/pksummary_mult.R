2006755a3e63682d47b3c64f23621e58c9a19905
Laura Shireman <laura.shireman@certara.com>
2022-06-01 08:42:19 -0700

    pksummary_mult: Added more arguments explicitly rather than only passing through to pksummary_table to make help file clearer and to autofill for ease of use.

--- R/pksummary_mult.R
+++ R/pksummary_mult.R
@@ -1,5 +1,10 @@
 #' Make PK summary tables from multiple simulator output files at once
 #'
+#' @param sim_data_files a character vector of simulator output files, e.g.,
+#'   \code{sim_data_files = c("My file 1.xlsx", "My file 2.xlsx")} or, if you
+#'   want all the Excel files in the current folder, \code{sim_data_files =
+#'   list.files("xlsx")}. If you supply something for \code{observed_PK}, you
+#'   can leave this unspecified.
 #' @param observed_PK (optional) If you have a data.frame or an Excel or csv
 #'   file with observed PK parameters, supply the data.frame or the full file
 #'   name in quotes here, and the simulated-to-observed mean ratios will be
@@ -43,8 +48,42 @@
 #'   the trials rather than the range of means.}} An example of acceptable input
 #'   here: \code{c("AUCtau_last", "AUCtau_last_withInhib", "Cmax_last",
 #'   "Cmax_last_withInhib", "AUCtau_ratio_last", "Cmax_ratio_last")}.
-#' @param ... other arguments passed to the function
-#'   \code{\link{pksummary_table}}
+#' @param PKorder Would you like the order of the PK parameters to be the the
+#'   order specified in the Consultancy Report Template (default), or would you
+#'   like the order to match the order you specified with the argument
+#'   \code{PKparameters}? Options are "default" or "user specified".
+#' @param sheet_PKparameters (optional) If you want the PK parameters to be
+#'   pulled from a specific tab in the simulator output file, list that tab
+#'   here. Most of the time, this should be left as NA.
+#' @param tissue For which tissue would you like the PK parameters to be pulled?
+#'   Options are "plasma" (default) or "blood" (possible but not as thoroughly
+#'   tested).
+#' @param mean_type return "arithmetic" or "geometric" (default) means and CVs.
+#'   If you supplied a report input form, only specify this if you'd like to
+#'   override the value listed there.
+#' @param includeTrialMeans TRUE or FALSE (default) for whether to include the
+#'   range of trial means for a given parameter. Note: This is calculated from
+#'   individual values rather than being pulled directly from the output.
+#' @param includeCV TRUE (default) or FALSE for whether to include rows for CV
+#'   in the table
+#' @param includeConfInt TRUE (default) or FALSE for whether to include whatever
+#'   confidence intervals were included in the simulator output file. Note that
+#'   the confidence intervals are geometric since that's what the simulator
+#'   outputs (see an AUC tab and the summary statistics; these values are the
+#'   ones for, e.g., "90\% confidence interval around the geometric mean(lower
+#'   limit)").
+#' @param includePerc TRUE or FALSE (default) for whether to include 5th to 95th
+#'   percentiles
+#' @param concatVariability TRUE or FALSE (default) for whether to concatenate
+#'   the variability. If "TRUE", the output will be formatted into a single row
+#'   and listed as the lower confidence interval or percentile to the upper CI
+#'   or percentile, e.g., "2400 to 2700". Please note that the current
+#'   SimcypConsultancy template lists one row for each of the upper and lower
+#'   values, so this should be set to FALSE for official reports.
+#' @param prettify_columns TRUE (default) or FALSE for whether to make easily
+#'   human-readable column names. TRUE makes pretty column names such as "AUCinf
+#'   (h*ng/mL)" whereas FALSE leaves the column with the R-friendly name from
+#'   \code{\link{extractPK}}, e.g., "AUCinf_dose1".
 #' @param checkDataSource TRUE (default) or FALSE for whether to include in the
 #'   output a data.frame that lists exactly where the data were pulled from the
 #'   simulator output file. Useful for QCing.
@@ -64,7 +103,16 @@
 pksummary_mult <- function(sim_data_files, 
                            observed_PK = NA,
                            PKparameters = NA,
-                           ..., 
+                           PKorder = "default", 
+                           sheet_PKparameters = NA, 
+                           mean_type = NA, 
+                           tissue = "plasma", 
+                           includeCV = TRUE, 
+                           includeConfInt = TRUE, 
+                           includePerc = FALSE, 
+                           includeTrialMeans = FALSE, 
+                           concatVariability = FALSE, 
+                           prettify_columns = TRUE, 
                            checkDataSource = TRUE, 
                            save_table = NA){
     
@@ -94,9 +142,21 @@ pksummary_mult <- function(sim_data_files,
     if(exists("observed_PKDF", inherits = FALSE)){
         
         for(i in 1:nrow(observed_PKDF)){
-            temp <- pksummary_table(observed_PK = observed_PKDF[i, ], 
-                                    checkDataSource = checkDataSource,
-                                    prettify_effector_name = "effector")
+            temp <- pksummary_table(#sim_data_file = NA,
+                observed_PK = observed_PKDF[i, ], 
+                PKparameters = PKparameters, 
+                PKorder = PKorder, 
+                sheet_PKparameters = sheet_PKparameters, 
+                mean_type = mean_type,
+                tissue = tissue, 
+                includeCV = includeCV,
+                includeConfInt = includeConfInt, 
+                includePerc = includePerc, 
+                includeTrialMeans = includeTrialMeans,
+                concatVariability = concatVariability,
+                prettify_columns = prettify_columns, 
+                checkDataSource = checkDataSource,
+                prettify_effector_name = "effector")
             
             OutPK[[i]] <- temp$Table %>% 
                 mutate(File = observed_PK$File[i])
@@ -113,6 +173,16 @@ pksummary_mult <- function(sim_data_files,
         for(i in sim_data_files){
             temp <- pksummary_table(sim_data_file = i, 
                                     PKparameters = PKparameters, 
+                                    PKorder = PKorder, 
+                                    sheet_PKparameters = sheet_PKparameters, 
+                                    mean_type = mean_type,
+                                    tissue = tissue, 
+                                    includeCV = includeCV,
+                                    includeConfInt = includeConfInt, 
+                                    includePerc = includePerc, 
+                                    includeTrialMeans = includeTrialMeans,
+                                    concatVariability = concatVariability,
+                                    prettify_columns = prettify_columns, 
                                     checkDataSource = checkDataSource,
                                     prettify_effector_name = "effector")
             
