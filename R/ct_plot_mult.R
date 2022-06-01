5e94ff58bc1f7b95f8fe9bef1a80e1b0a2e8181b
Laura Shireman <laura.shireman@certara.com>
2022-06-01 08:27:06 -0700

    ct_plot_mult: Added more arguments explicitly so that they're included in hte help file and will autofill. Trying to make things easier for user.

--- R/ct_plot_mult.R
+++ R/ct_plot_mult.R
@@ -22,6 +22,122 @@
 #'   graphs nicely arranged together. FALSE means that each Simulator output
 #'   file will have its own output file, and that graph's output file will be
 #'   named to match the Simulator output file name.
+#' @param figure_type type of figure to plot. Options are:
+#'
+#'   \describe{
+#'
+#'   \item{"trial means"}{plots an opaque line for the mean data, lighter lines
+#'   for the mean of each trial of simulated data, and open circles for the
+#'   observed data. If an effector were present, lighter dashed lines indicate
+#'   the mean of each trial of simulated data in the presence of the effector.}
+#'
+#'   \item{"percentiles"}{(default) plots an opaque line for the mean data,
+#'   lighter lines for the 5th and 95th percentiles of the simulated data, and
+#'   open circles for the observed data. If an effecter were present, the
+#'   default is dashed lines for the data in the presence of an effector.}
+#'
+#'   \item{"percentile ribbon"}{plots an opaque line for the mean data,
+#'   transparent shading for the 5th to 95th percentiles of the simulated data,
+#'   and open circles for the observed data. If an effector were present, the
+#'   default is to show the data without the effector in blue and the data in
+#'   the presence of the effector in red. Note: You may sometimes see some
+#'   artifacts -- especially for semi-log plots -- where the ribbon gets partly
+#'   cut off. For arcane reasons we don't want to bore you with here, we can't
+#'   easily prevent this. However, a possible fix is to set your y axis limits
+#'   for the semi-log plot to be wider using \code{y_axis_limits_log}.}
+#'
+#'   \item{"means only"}{plots a black line for the mean data and, if an
+#'   effector was modeled, a dashed line for the concentration-time data with
+#'   Inhibitor 1.}
+#'
+#'   \item{"Freddy"}{Freddy's favorite style of plot with trial means in light
+#'   gray, the overall mean in thicker black, the 5th and 95th percentiles in
+#'   dashed lines, and the observed data in semi-transparent purple-blue. Graphs
+#'   with an effector present lose the trial means, and the percentiles switch
+#'   to solid, gray lines. \strong{An editorial comment:} While this does not
+#'   align with the officially sanctioned template at this time, this looks
+#'   \emph{sharp}, makes it easy to see the defining characteristics of the
+#'   data, and I recommend checking it out, even just for your own purposes of
+#'   examining your data. If the color is too much for you but you like the
+#'   rest, try setting \code{obs_color = "none"}. -LS}}
+#'
+#' @param mean_type graph "arithmetic" (default) or "geometric" means or
+#'   "median" for median concentrations
+#' @param linear_or_log the type of graph to be returned. Options: "semi-log"
+#'   (default), "linear", "both vertical" (graphs are stacked vertically), or
+#'   "both horizontal" (graphs are side by side).
+#' @param time_range time range to show relative to the start of the simulation.
+#'   Options: \describe{
+#'
+#'   \item{NA}{(default) entire time range of data}
+#'
+#'   \item{a start time and end time in hours}{only data in that time range,
+#'   e.g. \code{c(24, 48)}. Note that there are no quotes around numeric data.}
+#'
+#'   \item{"first dose"}{only the time range of the first dose}
+#'
+#'   \item{"last dose"}{only the time range of the last dose}
+#'
+#'   \item{"penultimate dose"}{only the time range of the 2nd-to-last dose,
+#'   which can be useful for BID data where the end of the simulation extended
+#'   past the dosing interval or data when the substrate was dosed BID and the
+#'   effector was dosed QD}
+#'
+#'   \item{a specific dose number with "dose" or "doses" as the prefix}{the time
+#'   range encompassing the requested doses, e.g., \code{time_range = "dose 3"}
+#'   for the 3rd dose or \code{time_range = "doses 1 to 4"} for doses 1 to 4}
+#'
+#'   \item{"all obs" or "all observed" if you feel like spelling it out}{Time
+#'   range will be limited to only times when observed data are present.}
+#'
+#'   \item{"last dose to last observed" or "last obs" for short}{Time range will
+#'   be limited to the start of the last dose until the last observed data
+#'   point.}
+#'
+#'
+#'   }
+#'
+#' @param pad_x_axis optionally add a smidge of padding to the the x axis
+#'   (default is TRUE, which includes some generally reasonable padding). If
+#'   changed to FALSE, the y axis will be placed right at the beginning of your
+#'   time range and all data will end \emph{exactly} at the end of the time
+#'   range specified. If you want a \emph{specific} amount of x-axis padding,
+#'   set this to a number; the default is \code{c(0.02, 0.04)}, which adds 2\%
+#'   more space to the left side and 4\% more to the right side of the x axis.
+#'   If you only specify one number, we'll assume that's the percent you want
+#'   added to the left side.
+#' @param pad_y_axis optionally add a smidge of padding to the y axis (default
+#'   is TRUE, which includes some generally reasonable padding). As with
+#'   \code{pad_x_axis}, if changed to FALSE, the x axis will be placed right at
+#'   the bottom of your data, possible cutting a point in half. If you want a
+#'   \emph{specific} amount of y-axis padding, set this to a number; the default
+#'   is \code{c(0.02, 0)}, which adds 2\% more space to the bottom and nothing
+#'   to the top of the y axis. If you only specify one number, we'll assume
+#'   that's the percent you want added to the bottom.
+#' @param x_axis_interval optionally set the x-axis major tick-mark interval.
+#'   Acceptable input: any number or leave as NA to accept default values, which
+#'   are generally reasonable guesses as to aesthetically pleasing and
+#'   PK-relevant intervals.
+#' @param y_axis_limits_lin optionally set the Y axis limits for the linear
+#'   plot, e.g., \code{c(10, 1000)}. If left as the default NA, the Y axis
+#'   limits for the linear plot will be automatically selected.
+#' @param y_axis_limits_log optionally set the Y axis limits for the semi-log
+#'   plot, e.g., \code{c(10, 1000)}. Values will be rounded down and up,
+#'   respectively, to a round number. If left as the default NA, the Y axis
+#'   limits for the semi-log plot will be automatically selected.
+#' @param legend_position Specify where you want the legend to be. Options are
+#'   "left", "right", "bottom", "top", or "none" (default) if you don't want one
+#'   at all. If you include the legend but then some graphs do have a legend and
+#'   some graphs do not (e.g., some have effectors and some do not so there's
+#'   nothing to put in a legend), the alignment between sets of graphs will be a
+#'   bit off.
+#' @param legend_label optionally indicate on the legend whether the effector is
+#'   an inhibitor, inducer, activator, or suppressor. Input will be used as the
+#'   label in the legend for the line style and the shape. If left as the
+#'   default NA when a legend is included and an effector is present, the label
+#'   in the legend will be "Inhibitor".
+#' @param include_title TRUE or FALSE (default) on whether to include a title
+#'   for each small graph.
 #' @param file_labels optionally specify a label to be used for the file name in
 #'   the graphs (this applies when \code{include_title = TRUE}) and/or specify
 #'   the order in which the files are graphed with a named character vector of
@@ -42,18 +158,7 @@
 #' @param ncol number of columns of small graphs in the arrangement. If left as
 #'   NA, a reasonable guess will be made. (Not applicable if
 #'   \code{single_out_file = FALSE}.)
-#' @param linear_or_log the type of graph to be returned. Options: "semi-log"
-#'   (default), "linear", "both vertical" (graphs are stacked vertically), or
-#'   "both horizontal" (graphs are side by side).
 #' @param ... arguments that pass through to \code{\link{ct_plot}}
-#' @param include_title TRUE or FALSE (default) on whether to include a title
-#'   for each small graph.
-#' @param legend_position Specify where you want the legend to be. Options are
-#'   "left", "right", "bottom", "top", or "none" (default) if you don't want one
-#'   at all. If you include the legend but then some graphs do have a legend and
-#'   some graphs do not (e.g., some have effectors and some do not so there's
-#'   nothing to put in a legend), the alignment between sets of graphs will be a
-#'   bit off.
 #' @param save_graph optionally save the output graph by supplying a file name
 #'   in quotes here, e.g., "My conc time graph.png". If you leave off ".png", it
 #'   will be saved as a png file, but if you specify a different file extension,
@@ -83,13 +188,22 @@
 #' 
 ct_plot_mult <- function(ct_dataframe, 
                          single_out_file = TRUE, 
+                         figure_type = "percentiles",
+                         mean_type = "arithmetic",
                          linear_or_log = "semi-log",
-                         graph_labels = TRUE,
+                         time_range = NA, 
+                         x_axis_interval = NA, 
+                         pad_x_axis = TRUE, 
+                         pad_y_axis = TRUE,
+                         y_axis_limits_lin = NA, 
+                         y_axis_limits_log = NA, 
+                         legend_position = "none",
+                         legend_label = NA, 
                          include_title = FALSE,
                          file_labels = NA,
-                         legend_position = "none",
                          nrow = NULL, 
                          ncol = NULL,
+                         graph_labels = TRUE,
                          save_graph = NA,
                          fig_height = 8,
                          fig_width = 8,
@@ -196,11 +310,20 @@ ct_plot_mult <- function(ct_dataframe,
             
             AllGraphs[[i]] <- 
                 ct_plot(ct_dataframe = AllData[[i]], 
-                        ..., # comment this when developing
-                        include_graph_labels = FALSE,
-                        include_legend = ifelse(legend_position == "none",
-                                                FALSE, TRUE),
-                        linear_or_log = linear_or_log)
+                        figure_type = figure_type,
+                        mean_type = mean_type,
+                        linear_or_log = linear_or_log,
+                        time_range = time_range, 
+                        x_axis_interval = x_axis_interval, 
+                        pad_x_axis = pad_x_axis, 
+                        pad_y_axis = pad_y_axis,
+                        y_axis_limits_lin = y_axis_limits_lin, 
+                        y_axis_limits_log = y_axis_limits_log, 
+                        legend_position = legend_position,
+                        legend_label = legend_label, 
+                        graph_labels = graph_labels, 
+                        ... # comment this when developing
+                )
             
             if(include_title){
                 # This is for when they only want one of linear or log.
@@ -219,17 +342,42 @@ ct_plot_mult <- function(ct_dataframe,
                         # linear plot on top
                         ct_plot(ct_dataframe = AllData[[i]], 
                                 include_graph_labels = FALSE,
-                                ..., # comment this when developing
-                                linear_or_log = "linear") +
+                                figure_type = figure_type,
+                                linear_or_log = "linear",
+                                mean_type = mean_type,
+                                time_range = time_range, 
+                                x_axis_interval = x_axis_interval, 
+                                pad_x_axis = pad_x_axis, 
+                                pad_y_axis = pad_y_axis,
+                                y_axis_limits_lin = y_axis_limits_lin, 
+                                y_axis_limits_log = y_axis_limits_log, 
+                                legend_position = legend_position,
+                                legend_label = legend_label, 
+                                graph_labels = graph_labels, 
+                                ... # comment this when developing
+                                ) +
                             ggtitle(Title_i) + 
                             theme(title = element_text(size = 10), 
                                   plot.margin = unit(c(0, 0, 0, 0), "lines")), 
                         
                         # semi-log on bottom
                         ct_plot(ct_dataframe = AllData[[i]], 
+                                graph_labels = FALSE,
                                 include_graph_labels = FALSE,
-                                ..., # comment this when developing
-                                linear_or_log = "semi-log")),
+                                figure_type = figure_type,
+                                linear_or_log = "semi-log",
+                                mean_type = mean_type,
+                                time_range = time_range, 
+                                x_axis_interval = x_axis_interval, 
+                                pad_x_axis = pad_x_axis, 
+                                pad_y_axis = pad_y_axis,
+                                y_axis_limits_lin = y_axis_limits_lin, 
+                                y_axis_limits_log = y_axis_limits_log, 
+                                legend_position = legend_position,
+                                legend_label = legend_label, 
+                                graph_labels = graph_labels, 
+                                ... # comment this when developing
+                        )),
                     
                     align = "hv", 
                     nrow = ifelse(linear_or_log %in% c("both", "both vertical"), 
