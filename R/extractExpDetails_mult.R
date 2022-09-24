9cb690370961291be51e43d2987cb941f4ebeab4
Laura Shireman <laura.shireman@certara.com>
2022-09-23 22:22:10 -0700

    extractExpDetails_mult: Fixing error I introduced with file order.

--- R/extractExpDetails_mult.R
+++ R/extractExpDetails_mult.R
@@ -162,10 +162,13 @@ extractExpDetails_mult <- function(sim_data_files = NA,
             if(overwrite == FALSE){
                 sim_data_files_topull <- setdiff(sim_data_files, 
                                                  existing_exp_details$File)
+                FileOrder <- unique(sim_data_files, existing_exp_details$File)
+                
             } else {
                 sim_data_files_topull <- sim_data_files
                 existing_exp_details <- existing_exp_details %>%
                     filter(!File %in% existing_exp_details$File)
+                FileOrder <- unique(sim_data_files, existing_exp_details$File)
             }
             
             FileOrder <- unique(c(FileOrder, sim_data_files_topull))
