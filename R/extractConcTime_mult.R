da76dc082f4b67592d00f7241da1d106fbaa4bb3
Laura Shireman <laura.shireman@certara.com>
2022-10-20 08:49:24 -0700

    extractConcTime_mult: Fixing issue with extractConcTime_mult where things were failing if any of the files were, say, sensitivity analyses.

--- R/extractConcTime_mult.R
+++ R/extractConcTime_mult.R
@@ -276,6 +276,16 @@ extractConcTime_mult <- function(sim_data_files = NA,
         # Getting summary data for the simulation(s)
         Deets <- extractExpDetails(ff, exp_details = "Input Sheet")
         
+        if(length(Deets) == 0){
+            # Using "warning" instead of "stop" here b/c I want this to be able to
+            # pass through to other functions and just skip any files that
+            # aren't simulator output.
+            warning(paste("The file", ff,
+                          "does not appear to be a Simcyp Simulator output Excel file. We cannot return any information for this file."), 
+                    call. = FALSE)
+            next()
+        }
+        
         # Names of compounds requested for checking whether the data exist
         CompoundCheck <- c("substrate" = Deets$Substrate,
                            "inhibitor 1" = Deets$Inhibitor1,
