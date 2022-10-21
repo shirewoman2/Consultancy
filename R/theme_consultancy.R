4efe25ce9e3f49bb3afa5a368aad2edd3d19ff75
Laura Shireman <laura.shireman@certara.com>
2022-10-21 12:57:54 -0700

    theme_consultancy: Tweaking help file text

--- R/theme_consultancy.R
+++ R/theme_consultancy.R
@@ -8,7 +8,7 @@
 #' line on the y axis but no overall border around the graph.} \item{If the
 #' graph is facetted, the labels for the facets are not enclosed with a border.}
 #' \item{Axis titles and, when applicable, the plot title are bold face.}
-#' \item{Font sizes and margins are the same as in
+#' \item{Font sizes and margins are the same as in theme_bw in
 #' \code{\link{ggplot2::ggtheme}}} }
 #'
 #' @return
