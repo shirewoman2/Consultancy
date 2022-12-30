dc376831dbf97268ceaeaa4921e7aea20437872d
Laura Shireman <laura.shireman@certara.com>
2022-12-29 18:11:17 -0800

    extractConcTime_mult: Clarifying a few things; catching a few possible errors, etc.

--- R/extractConcTime_mult.R
+++ R/extractConcTime_mult.R
@@ -51,15 +51,15 @@
 #'
 #'   \item{a data.frame with one column for the observed files and one column
 #'   for the simulated files they each match}{The data.frame must have column
-#'   names of "ObsFile" and "SimFile" for the observed and simulated files,
+#'   names of "ObsFile" and "File" for the observed and simulated files,
 #'   respectively. Here's an example of acceptable input:
 #'   \code{obs_to_sim_assignment = data.frame(ObsFile = c("obsdata1.xlsx",
-#'   "obsdata2.xlsx"), SimFile = c("simfileA.xlsx", "simfileB.xlsx"))} Each row
+#'   "obsdata2.xlsx"), File = c("simfileA.xlsx", "simfileB.xlsx"))} Each row
 #'   should contain one observed file and one simulated file, so if you want to
 #'   compare a single observed file to multiple simulated files, you'll need to
 #'   repeat the observed file, e.g., \code{obs_to_sim_assignment =
 #'   data.frame(ObsFile = c("obsdata1.xlsx", "obsdata2.xlsx", "obsdata2.xlsx",
-#'   "obsdata2.xlsx"), SimFile = c("simfileA.xlsx", "simfileB.xlsx",
+#'   "obsdata2.xlsx"), File = c("simfileA.xlsx", "simfileB.xlsx",
 #'   "simfileC.xlsx", "simfileD.xlsx"))}}
 #'
 #'   \item{a csv file with one column for the observed files and one column for
@@ -282,14 +282,15 @@ extractConcTime_mult <- function(sim_data_files = NA,
                 # assignments.
                 ObsAssign <- read.csv(obs_to_sim_assignment)
             } else {
-                # Separating obs_to_sim_assignment so that it will work well with
-                # each simulator file. I wanted this to be obs 1st and then sim 2nd
-                # b/c it will often be the case that you would want to compare
-                # multiple sim files to the same obs data, so I wanted the value
-                # (sim file) to be able to be something convoluted with commas I
-                # could separate. For *here*, though, inside the actual function, it
-                # works better if things are named by the sim file. Splitting up the
-                # character vector to get things separated by sim file.
+                # Separating obs_to_sim_assignment so that it will work well
+                # with each simulator file. I wanted this to be obs 1st and then
+                # sim 2nd b/c it will often be the case that you would want to
+                # compare multiple sim files to the same obs data, so I wanted
+                # the value (sim file) to be able to be something convoluted
+                # with commas I could separate. For *here*, though, inside the
+                # actual function, it works better if things are named by the
+                # sim file. Splitting up the character vector to get things
+                # separated by sim file.
                 
                 # Making sure that the split pattern will work in case the user
                 # omitted spaces.
@@ -312,7 +313,6 @@ extractConcTime_mult <- function(sim_data_files = NA,
             # This is when the user has supplied a data.frame for
             # obs_to_sim_assignment.
             ObsAssign <- obs_to_sim_assignment
-            
         }
         
         # Tidying up a few things. Checking column names and dealing with any
@@ -326,46 +326,49 @@ extractConcTime_mult <- function(sim_data_files = NA,
             ObsAssign <- ObsAssign %>% select(-File) %>% rename(FILE = SIMFILE)
         }
         
-        # Now that column names should be correct, converting to the case I like
-        # for ease of coding
-        ObsAssign <- ObsAssign %>% rename(File = FILE, ObsFile = OBSFILE) %>% 
-            select(File, ObsFile)
-        
-        if(any(duplicated(ObsAssign$File))){ 
-            Dups <- ObsAssign$File[duplicated(ObsAssign$File)]
-            warning(paste0("You have more than one observed data file assigned to the simulator files ",
-                           str_comma(paste0("`", Dups, "`")),
-                           ". This function can only handle one observed file per simulator file, so only the first observed file listed will be used."),
+        if(all(c("FILE", "OBSFILE") %in% names(ObsAssign)) == FALSE){
+            warning("You have specified values for `obs_to_sim_assignment`, but it's not clear which should be for the observed files and which for the simulated files. Please check the help file for acceptable input. For now, we will not extract data from any observed data files.", 
                     call. = FALSE)
-            ObsAssign <- ObsAssign[!duplicated(ObsAssign$File), ]
-        }
-        
-        if(any(complete.cases(ObsAssign$File))){
-            MissingFiles <- setdiff(ObsAssign$File,
-                                    unique(c(sim_data_files, ct_dataframe$File)))
-            if(length(MissingFiles) > 0){
-                warning(paste0("When you assigned observed data files to simulator files with the argument `obs_to_sim_assignment`, you included simulator files that are *not* included in `sim_data_files`. We cannot include these observed data files in the output data because we don't know which simulator files they belong with. The problem simulator files is/are: ", 
-                               str_comma(MissingFiles), ", which is/are set to match the following observed files ",
-                               str_comma(names(obs_to_sim_assignment[
-                                   which(obs_to_sim_assignment %in%
-                                             unique(c(sim_data_files, ct_dataframe$File)) == FALSE)])), 
-                               "."), 
+            ObsAssign <- NA
+        } else {
+            
+            # Now that column names should be correct, converting to the case I like
+            # for ease of coding
+            ObsAssign <- ObsAssign %>% rename(File = FILE, ObsFile = OBSFILE) %>% 
+                select(File, ObsFile)
+            
+            if(any(duplicated(ObsAssign$File))){ 
+                Dups <- ObsAssign$File[duplicated(ObsAssign$File)]
+                warning(paste0("You have more than one observed data file assigned to the simulator files ",
+                               str_comma(paste0("`", Dups, "`")),
+                               ". This function can only handle one observed file per simulator file, so only the first observed file listed will be used."),
                         call. = FALSE)
-                
-                ObsAssign <- ObsAssign %>% filter(File %in% unique(c(sim_data_files, ct_dataframe$File)))
-                # if(class(obs_to_sim_assignment)[1] == "character"){
-                #     obs_to_sim_assignment <- obs_to_sim_assignment[
-                #         !str_detect(obs_to_sim_assignment, 
-                #                     str_c(MissingFiles, "|"))
-                #         ]
-                # } 
+                ObsAssign <- ObsAssign[!duplicated(ObsAssign$File), ]
             }
             
-            ObsAssign <- split(ObsAssign, f = ObsAssign$File)
-        } else {
-            ObsAssign <- list() # This is for when all sim files should use the same obs file
-        }
-    } 
+            if(any(complete.cases(ObsAssign$File))){
+                MissingFiles <- setdiff(ObsAssign$File,
+                                        unique(c(sim_data_files, ct_dataframe$File)))
+                if(length(MissingFiles) > 0){
+                    warning(paste0("When you assigned observed data files to simulator files with the argument `obs_to_sim_assignment`, you included simulator files that are *not* included in `sim_data_files`. We cannot include these observed data files in the output data because we don't know which simulator files they belong with. The problem simulator files is/are: ", 
+                                   str_comma(MissingFiles), ", which is/are set to match the following observed files ",
+                                   str_comma(names(obs_to_sim_assignment[
+                                       which(obs_to_sim_assignment %in%
+                                                 unique(c(sim_data_files, ct_dataframe$File)) == FALSE)])), 
+                                   "."), 
+                            call. = FALSE)
+                    
+                    ObsAssign <-
+                        ObsAssign %>% 
+                        filter(File %in% unique(c(sim_data_files, ct_dataframe$File)))
+                }
+                
+                ObsAssign <- split(ObsAssign, f = ObsAssign$File)
+            } else {
+                ObsAssign <- list() # This is for when all sim files should use the same obs file
+            }
+        } 
+    }
     
     
     ## Start of loop through files ------------------------------------------
