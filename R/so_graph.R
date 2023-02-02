b3d0af20f65e13f39d8c1d0fe6c4a671bd7aa3fc
Laura Shireman <laura.shireman@certara.com>
2023-02-02 12:57:22 -0800

    so_graph: Added some options for saving, plotting multiple PK parameters at once

--- R/so_graph.R
+++ R/so_graph.R
@@ -6,9 +6,10 @@
 #'
 #' @param PKtable a table in the same format as output from the function
 #'   \code{\link{pksummary_mult}}
-#' @param PKparameter any of the PK parameters included in the output from
-#'   \code{\link{pksummary_mult}}. To see the full set of possible parameters,
-#'   enter \code{view(PKParameterDefinitions)} into the console.
+#' @param PKparameters any of the PK parameters included in the output from
+#'   \code{\link{pksummary_mult}}; if left as NA, this will make graphs for each
+#'   parameter included in \code{PKtable}. To see the full set of possible
+#'   parameters, enter \code{view(PKParameterDefinitions)} into the console.
 #' @param color_set set of colors to use for indicating the 1.5-fold and 2-fold
 #'   boundaries of the simulated / observed ratio. The default is "red green",
 #'   "muted red green" will result in lighter, somewhat more muted red and green
@@ -21,6 +22,10 @@
 #'   "fill" to get a shaded area, or "none" to remove any indicators of those
 #'   boundaries.
 #' @param line_width line width; default is 0.8
+#' @param save_graph optionally save the output graph by supplying a file name
+#'   in quotes here, e.g., "My conc time graph.png"
+#' @param fig_height figure height in inches; default is 8
+#' @param fig_width figure width in inches; default is 6
 #'
 #' @return
 #' @export
@@ -29,10 +34,13 @@
 #' 
 
 so_graph <- function(PKtable, 
-                     PKparameter, 
+                     PKparameters = NA, 
                      color_set = "red green", 
                      boundary_indicator = "line",
-                     line_width = 0.8){
+                     line_width = 0.8, 
+                     save_graph = NA, 
+                     fig_width = 8, 
+                     fig_height = 6){
     
     # Error catching ----------------------------------------------------------
     # Check whether tidyverse is loaded
@@ -49,8 +57,13 @@ so_graph <- function(PKtable,
              call. = FALSE)
     }
     
-    if(any(str_detect(PKparameter, "ratio"))){
-        warning("You've requested a simulated vs. observed graph for a geometric mean ratio, which means that you really should make a graph like that in Guest Galetin 2011 Drug Metab Dispos paper, but we haven't that up yet.", 
+    if(is.na(PKparameters)){
+        PKparameters <- names(PKtable)
+        PKparameters <- PKparameters[!str_detect(PKparameters, "Statistic|File")]
+    }
+    
+    if(any(str_detect(PKparameters, "ratio"))){
+        warning("You've requested a simulated vs. observed graph for a geometric mean ratio, which means that you really should make a graph like that in Guest Galetin 2011 Drug Metab Dispos, but we haven't set that up yet.", 
                 call. = FALSE)
     }
     
@@ -66,18 +79,19 @@ so_graph <- function(PKtable,
     
     # Checking whether PK parameters are, in fact, prettified
     if(any(str_detect(SO$PrettifiedNames, " "))){
-        SO <- SO %>% 
-            left_join(AllPKParameters %>% select(PrettifiedNames, PKparameter) %>% 
-                          unique())
+        suppressMessages(
+            SO <- SO %>% 
+                left_join(AllPKParameters %>% select(PrettifiedNames, PKparameter) %>% 
+                              unique())
+        )
     } else {
         SO <- SO %>% mutate(PKparameter = PrettifiedNames)
     }
     
     SO <- SO %>% 
-        select(-PrettifiedNames) %>% 
         pivot_wider(names_from = Statistic, values_from = Value) %>% 
         filter(complete.cases(Observed) & 
-                   PKparameter %in% {{PKparameter}})
+                   PrettifiedNames %in% {{PKparameters}})
     
     Fold1.5_upper <- data.frame(Observed = 10^seq(-3, 6, length.out = 100)) %>% 
         mutate(LimitName = "upper", 
@@ -95,10 +109,13 @@ so_graph <- function(PKtable,
     Unity <- data.frame(Observed = 10^seq(-3, 6, length.out = 100)) %>% 
         mutate(Simulated = Observed)
     
-    Limits <- c(
-        round_down(min(c(SO$Observed, SO$Simulated), na.rm = T)),
-        
-        round_up(max(c(SO$Observed, SO$Simulated), na.rm = T)))
+    # Setting up polygons
+    Poly2x <- Fold2_upper %>% arrange(Observed) %>% 
+        bind_rows(Fold2_lower %>% arrange(desc(Observed)))
+    
+    Poly1.5x <- Fold1.5_upper %>% arrange(Observed) %>% 
+        bind_rows(Fold1.5_lower %>% arrange(desc(Observed)))
+    
     
     if(length(color_set) == 1){
         BoundColors <- switch(color_set, 
@@ -109,54 +126,94 @@ so_graph <- function(PKtable,
         BoundColors <- color_set[1:2]
     }
     
-    # Setting up polygongs
-    Poly2x <- Fold2_upper %>% arrange(Observed) %>% 
-        bind_rows(Fold2_lower %>% arrange(desc(Observed)))
-    
-    Poly1.5x <- Fold1.5_upper %>% arrange(Observed) %>% 
-        bind_rows(Fold1.5_lower %>% arrange(desc(Observed)))
     
     
-    G <- ggplot(SO, aes(x = Observed, y = Simulated)) +
-        geom_line(data = Unity, linetype = "dashed", linewidth = line_width)
+    G <- list()
+    SO <- split(SO, f = SO$PKparameter)
     
-    if(str_detect(boundary_indicator, "line")){
-        G <- G + 
-            geom_line(data = Fold2_upper, color = BoundColors[1], linewidth = line_width) +
-            geom_line(data = Fold2_lower, color = BoundColors[1], linewidth = line_width) +
-            geom_line(data = Fold1.5_upper, color = BoundColors[2], linewidth = line_width) +
-            geom_line(data = Fold1.5_lower, color = BoundColors[2], linewidth = line_width) 
+    for(i in names(SO)){
+        
+        Limits <- c(
+            round_down(min(c(SO[[i]]$Observed, SO[[i]]$Simulated), na.rm = T)),
+            
+            round_up(max(c(SO[[i]]$Observed, SO[[i]]$Simulated), na.rm = T)))
+        
+        G[[i]] <- ggplot(SO[[i]], aes(x = Observed, y = Simulated)) +
+            geom_line(data = Unity, linetype = "dashed", linewidth = line_width)
+        
+        if(str_detect(boundary_indicator, "line")){
+            G[[i]] <- G[[i]] + 
+                geom_line(data = Fold2_upper, color = BoundColors[1], linewidth = line_width) +
+                geom_line(data = Fold2_lower, color = BoundColors[1], linewidth = line_width) +
+                geom_line(data = Fold1.5_upper, color = BoundColors[2], linewidth = line_width) +
+                geom_line(data = Fold1.5_lower, color = BoundColors[2], linewidth = line_width) 
+        }
+        
+        if(str_detect(boundary_indicator, "fill")){
+            G[[i]] <- G[[i]] +
+                geom_polygon(data = Poly2x, fill = BoundColors[1]) +
+                geom_polygon(data = Poly1.5x, fill = BoundColors[2])
+        }
+        
+        G[[i]] <- G[[i]] + geom_point(size = 2) +
+            scale_y_log10(
+                breaks = c(10^(-1:4),
+                           3*10^(-1:4),
+                           5*10^(-1:4)),
+                minor_breaks = rep(1:9)*rep(10^(-1:4), each = 9)) +
+            scale_x_log10(
+                breaks = c(10^(-1:4),
+                           3*10^(-1:4),
+                           5*10^(-1:4)),
+                minor_breaks = rep(1:9)*rep(10^(-1:4), each = 9)) +
+            coord_cartesian(xlim = Limits, ylim = Limits) +
+            ggtitle(PKexpressions[[i]]) +
+            theme_bw() +
+            theme(aspect.ratio = 1, 
+                  plot.title = element_text(hjust = 0.5),
+                  axis.title = element_text(color = "black", face = "bold"),
+                  axis.title.x = element_text(margin = margin(2.75, 0, 0, 0)),
+                  axis.title.x.top = element_text(margin = margin(0, 0, 2.75, 0)),
+                  axis.title.y = element_text(margin = margin(0, 2.75, 0, 0)),
+                  axis.title.y.right = element_text(margin = margin(0, 0, 0, 2.75)))
+        
     }
     
-    if(str_detect(boundary_indicator, "fill")){
-        G <- G +
-            geom_polygon(data = Poly2x, fill = BoundColors[1]) +
-            geom_polygon(data = Poly1.5x, fill = BoundColors[2])
+    if(length(G) == 1){
+        
+        G <- G[[1]]
+        
+    } else {
+        
+        G <-  ggpubr::ggarrange(plotlist = G, align = "hv")
+        
     }
     
-    G <- G + geom_point(size = 2) +
-        scale_y_log10(
-            breaks = c(10^(-1:4),
-                       3*10^(-1:4),
-                       5*10^(-1:4)),
-            minor_breaks = rep(1:9)*rep(10^(-1:4), each = 9)) +
-        scale_x_log10(
-            breaks = c(10^(-1:4),
-                       3*10^(-1:4),
-                       5*10^(-1:4)),
-            minor_breaks = rep(1:9)*rep(10^(-1:4), each = 9)) +
-        coord_cartesian(xlim = Limits, ylim = Limits) +
-        ggtitle(PKexpressions[[PKparameter]]) +
-        theme_bw() +
-        theme(aspect.ratio = 1, 
-              plot.title = element_text(hjust = 0.5),
-              axis.title = element_text(color = "black", face = "bold"),
-              axis.title.x = element_text(margin = margin(2.75, 0, 0, 0)),
-              axis.title.x.top = element_text(margin = margin(0, 0, 2.75, 0)),
-              axis.title.y = element_text(margin = margin(0, 2.75, 0, 0)),
-              axis.title.y.right = element_text(margin = margin(0, 0, 0, 2.75)))
+    
+    # Saving --------------------------------------------------------------
+    if(complete.cases(save_graph)){
+        FileName <- save_graph
+        if(str_detect(FileName, "\\.")){
+            # Making sure they've got a good extension
+            Ext <- sub("\\.", "", str_extract(FileName, "\\..*"))
+            FileName <- sub(paste0(".", Ext), "", FileName)
+            Ext <- ifelse(Ext %in% c("eps", "ps", "jpeg", "tiff",
+                                     "png", "bmp", "svg", "jpg"), 
+                          Ext, "png")
+            FileName <- paste0(FileName, ".", Ext)
+        } else {
+            FileName <- paste0(FileName, ".png")
+            Ext <- "png"
+        }
+        
+        # This is when they want any kind of graphical file format.
+        ggsave(FileName, height = fig_height, width = fig_width, dpi = 600,
+               plot = G)
+        
+    }
     
     return(G)
+    
 }
 
 
