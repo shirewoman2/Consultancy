721f440ca6a95cc3d2b79cfdf0811a4c5f99b432
Laura Shireman <laura.shireman@certara.com>
2023-09-06 14:02:25 -0700

    merging from beta

--- R/format_obs_for_XML.R
+++ R/format_obs_for_XML.R
@@ -4,7 +4,7 @@
 #' data into a format that works for pasting into the Simcyp Simulator PE data
 #' template, which is then used to generate an observed data overlay XML file.
 #' This should be used with one study cohort at at time, e.g., only one dose
-#' level and one dosing regimen. 
+#' level and one dosing regimen.
 #'
 #' @param obs_dataframe observed concentration-time data.frame
 #' @param conc_column the column in the observed concentration-time data.frame
@@ -13,42 +13,49 @@
 #'   that contains time data
 #' @param subject_column the column in the observed concentration-time
 #'   data.frame that contains the subject ID
-#' @param compound_dose_amount the amount of the dose. If this amount varies,
-#'   please include one dose amount for each time. For example:
-#'   \code{compound_dose_amount = c(100, 50, 50)} will generate doses of 100 mg
-#'   for the first dose and then 50 mg for the next two doses. \strong{An R
-#'   coding tip:} You don't \emph{have} to list everything multiple times; you
-#'   can use the function \code{\link{rep}} to repeat elements. For example,
-#'   here's how you could specify that the 1st dose should be 100 mg but the
-#'   next 10 doses should be 50: \code{compound_dose_amount = c(100, rep(50,
-#'   10))}
-#' @param compound_dose_unit the unit of dosing. Options are "mg" (default),
-#'   "mg/m2", or "mg/kg".
-#' @param compound_inf_duration the infusion duration (min) (optional)
-#' @param compound_start the start time of compound administration (h); default
-#'   is 0.
-#' @param compound_route the route of administration. Options are "Oral"
-#'   (default), "Intravenous", "Dermal", "Inhaled", "SC-First Order",
-#'   "SC-Mechanistic", or "Auto-detect". Not case sensitive.
-#' @param dose_interval the dosing interval in hours. Default is 24 for a QD
-#'   dosing regimen.
+#' @param DVID_column the column in the observed concentration-time data.frame
+#'   that indicates any time the DV number in the PE file should change. For
+#'   example, if you have some data in plasma and some in blood and have a
+#'   column called "Tissue", you'd want to set this as \code{DVID_column =
+#'   Tissue}. If you have an inhibitor present in some cases but not others, set
+#'   this to the column with the inhibitor data. Any time the value changes in
+#'   \code{obs_dataframe}, the number in the "DV" column will change in the
+#'   output. We'll include a message to indicate which value was used for which
+#'   DV number.
+#' @param dose the amount of the dose. If this amount varies, please include one
+#'   dose amount for each time. For example: \code{dose = c(100, 50, 50)} will
+#'   generate doses of 100 mg for the first dose and then 50 mg for the next two
+#'   doses. \strong{An R coding tip:} You don't \emph{have} to list everything
+#'   multiple times; you can use the function \code{\link{rep}} to repeat
+#'   elements. For example, here's how you could specify that the 1st dose
+#'   should be 100 mg but the next 10 doses should be 50: \code{dose = c(100,
+#'   rep(50, 10))}
+#' @param dose_unit the unit of dosing. Options are "mg" (default), "mg/m2", or
+#'   "mg/kg".
+#' @param inf_duration the infusion duration (min) (optional)
+#' @param dosing_start_time the start time of compound administration (h);
+#'   default is 0.
+#' @param dose_route the route of administration. Options are "Oral" (default),
+#'   "Intravenous", "Dermal", "Inhaled", "SC-First Order", "SC-Mechanistic", or
+#'   "Auto-detect". Not case sensitive.
+#' @param dose_interval the dosing interval in hours. Default is NA for a single
+#'   dose. Set this to, e.g., 24 for a QD dosing regimen.
 #' @param num_doses the number of doses to generate. If this is left as NA, then
 #'   the value for \code{end_time} will be used to determine the number of doses
 #'   administered.
-#' @param end_time the end time of the dosing in hours. If \code{num_doses} is
+#' @param end_time the end time of the study in hours. If \code{num_doses} is
 #'   filled out, that value will be used preferentially.
 #' @param custom_dosing_schedule a custom dosing schedule to be used for each
 #'   subject in hours, e.g., \code{custom_dosing_schedule = c(0, 12, 24, 168,
 #'   180, 192)}; if this is filled out, values in \code{dose_interval},
 #'   \code{num_doses}, and \code{end_time} will all be ignored.
 #' @param simulator_version the version of the simulator that will be used. This
-#'   affects what columns will be included in hte output.
-#' @param compound_ID specify the compound that's being dosed. Options are
+#'   affects what columns will be included in the output.
+#' @param compoundID specify the compound that's being dosed. Options are
 #'   "Substrate" (default), "Inhibitor 1", "Inhibitor 2", or "Inhibitor 3". Not
 #'   case sensitive. If you list more than one compound, you must also list more
-#'   than one \code{compound_route}, \code{compound_dose_unit}, and
-#'   \code{compound_dose_amount} or list just one of each with the understanding
-#'   that they will all be the same.
+#'   than one \code{dose_route}, \code{dose_unit}, and \code{dose} or list just
+#'   one of each with the understanding that they will all be the same.
 #' @param demog_dataframe a data.frame of demographic information (optional)
 #' @param age_column the column in demog_dataframe that contains age data
 #' @param weight_column the column in demog_dataframe that contains weight data
@@ -69,23 +76,25 @@ format_obs_for_XML <- function(obs_dataframe,
                                conc_column, 
                                time_column, 
                                subject_column, 
-                               compound_dose_amount = 100,
-                               compound_dose_unit = "mg",
-                               compound_inf_duration = NA,
-                               compound_start = 0,
-                               compound_route = "Oral",
-                               dose_interval = 24, 
+                               DVID_column,
+                               dose = 100,
+                               dose_unit = "mg",
+                               inf_duration = NA,
+                               dosing_start_time = 0,
+                               dose_route = "Oral",
+                               dose_interval = NA, 
                                num_doses = NA,
                                end_time = NA,
                                custom_dosing_schedule = NA,
                                simulator_version = 22,
-                               compound_ID = "Substrate",
+                               compoundID = "Substrate",
                                demog_dataframe, # should have same column name for subject IDs as in obs_dataframe.
                                age_column, 
                                weight_column, 
                                height_column, 
                                sex_column, 
-                               save_output = NA){
+                               save_output = NA, 
+                               return_data = FALSE){
    
    # Error catching ----------------------------------------------------------
    # Check whether tidyverse is loaded
@@ -97,6 +106,7 @@ format_obs_for_XML <- function(obs_dataframe,
    conc_column <- rlang::enquo(conc_column)
    time_column <- rlang::enquo(time_column)
    subject_column <- rlang::enquo(subject_column)
+   DVID_column <- rlang::enquo(DVID_column)
    age_column <- rlang::enquo(age_column)
    weight_column <- rlang::enquo(weight_column)
    height_column <- rlang::enquo(height_column)
@@ -107,6 +117,7 @@ format_obs_for_XML <- function(obs_dataframe,
    GoodNames_step1 <- c(Conc = rlang::as_label(conc_column),
                         Time = rlang::as_label(time_column),
                         Subject = rlang::as_label(subject_column), 
+                        DVID = rlang::as_label(DVID_column),
                         Age = rlang::as_label(age_column), 
                         Weight = rlang::as_label(weight_column), 
                         Height = rlang::as_label(height_column), 
@@ -118,12 +129,18 @@ format_obs_for_XML <- function(obs_dataframe,
       select(any_of(c(rlang::as_label(conc_column),
                       rlang::as_label(time_column),
                       rlang::as_label(subject_column), 
+                      rlang::as_label(DVID_column),
                       rlang::as_label(age_column), 
                       rlang::as_label(weight_column), 
                       rlang::as_label(height_column), 
                       rlang::as_label(sex_column)))) %>% 
       rename_with(~str_replace_all(., GoodNames)) %>% 
       unique()
+   
+   if("Sex" %in% names(obs_dataframe)){
+      obs_dataframe <- obs_dataframe %>% 
+         mutate(Sex = str_sub(str_to_upper(Sex), 1, 1))
+   }
    
    # MissingCols <- setdiff(
    #    CheckNames[CheckNames != "<empty>" &
@@ -167,6 +184,10 @@ format_obs_for_XML <- function(obs_dataframe,
          rename_with(~str_replace_all(., GoodNames)) %>% 
          unique()
       
+      if("Sex" %in% names(demog_dataframe)){
+         demog_dataframe <- demog_dataframe %>%  
+            mutate(Sex = str_sub(str_to_upper(subj_sex), 1, 1))
+      }
    }
    
    MissingCols <- setdiff(c("Subject", "Age", "Weight", "Height", "Sex"), 
@@ -181,33 +202,53 @@ format_obs_for_XML <- function(obs_dataframe,
    
    # Main body of function ---------------------------------------------------
    
+   FinalObsDF <- obs_dataframe %>% 
+      # filtering to remove NAs here but could return to this to
+      # allow user to choose what option they want instead. Could
+      # make it either remove NAs or set to 0 or some other value
+      # s/a 1/2 LLOQ.
+      filter(complete.cases(Conc)) %>% 
+      select(Subject, Time, Conc, DVID) %>% 
+      left_join(demog_dataframe %>%
+                   select(Subject, Age, Height, Weight, Sex), 
+                by = "Subject") %>% 
+      mutate(across(.cols = everything(), .fns = as.character), 
+             DVID = as.factor(DVID)) %>% 
+      rename("Subject ID" = Subject,
+             "Age (year)" = Age, 
+             "Weight (kg)" = Weight, 
+             "Height (cm)" = Height, 
+             "DV ID" = DVID,
+             DV = Conc)
+   
+   if(all(is.na(FinalObsDF$`DV ID`))){
+      FinalObsDF$`DV ID` <- 1
+   } else {
+      MyDVIDs <- data.frame(DVIDvalue = levels(FinalObsDF$`DV ID`)) %>% 
+         mutate(DVID = 1:nrow(.), 
+                Message = paste(DVID, "=", DVIDvalue))
+      
+      message("DV IDs in the output file are:\n",
+              str_c(MyDVIDs$Message, separate = "\n"))
+   }
+      
    Out <- create_doses(
       dose_interval = dose_interval,
+      compound_dosing_start_time = dosing_start_time,
+      num_doses = num_doses, 
       end_time = end_time, 
-      compound_dose_amount = compound_dose_amount, 
+      compound_dose_amount = dose, 
+      compound_dose_route = dose_route,
+      compound_inf_duration = inf_duration,
+      compound_dose_unit = dose_unit,
       subj_ID = demog_dataframe$Subject, 
       subj_age = demog_dataframe$Age, 
       subj_height = demog_dataframe$Height, 
       subj_weight = demog_dataframe$Weight,
       subj_sex = demog_dataframe$Sex, 
       simulator_version = simulator_version) %>% 
-      bind_rows(obs_dataframe %>% 
-                   # filtering to remove NAs here but could return to this to
-                   # allow user to choose what option they want instead. Could
-                   # make it either remove NAs or set to 0 or some other value
-                   # s/a 1/2 LLOQ.
-                   filter(complete.cases(Conc)) %>% 
-                   select(Subject, Time, Conc) %>% 
-                   left_join(demog_dataframe %>%
-                                select(Subject, Age, Height, Weight, Sex), 
-                             by = "Subject") %>% 
-                   rename("Subject ID" = Subject,
-                          "Age (year)" = Age, 
-                          "Weight (kg)" = Weight, 
-                          "Height (cm)" = Height, 
-                          DV = Conc) %>%
-                   mutate("DV ID" = 1,
-                          across(.cols = everything(), .fns = as.character))) %>% 
+      bind_rows(FinalObsDF %>% 
+                   mutate(`DV ID` = as.character(as.numeric(`DV ID`)))) %>% 
       mutate(across(.cols = everything(), .fns = as.character), 
              across(.cols = everything(), .fns = function(x) ifelse(is.na(x), "", x)))
    
