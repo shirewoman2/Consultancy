258e24a73a9fd819c11b1e67bb545f468531b5f0
Laura Shireman <laura.shireman@certara.com>
2023-12-15 06:30:55 -0800

    prettify_column_names: Expanded options to now also uglify col names on request to make them R friendly.

--- R/prettify_column_names.R
+++ R/prettify_column_names.R
@@ -7,22 +7,31 @@
 #'
 #' @param PKtable a table of PK data with column names such as "AUCinf_dose1" or
 #'   "Cmax_last"
-#' @param prettify_compound_names TRUE (default) or FALSE on whether to make
-#'   compound names prettier in the prettified column titles and in any Word
-#'   output files. This was designed for simulations where the substrate and any
-#'   metabolites, perpetrators, or perpetrator metabolites are among the standard
-#'   options for the simulator, and leaving \code{prettify_compound_names =
-#'   TRUE} will make the name of those compounds something more human readable.
-#'   For example, "SV-Rifampicin-MD" will become "rifampicin", and
-#'   "Sim-Midazolam" will become "midazolam". Set each compound to the name
-#'   you'd prefer to see in your column titles if you would like something
-#'   different. For example, \code{prettify_compound_names = c("perpetrator" =
+#' @param prettify_compound_names Do you want to prettify your compound
+#'   names? Options: \describe{
+#'
+#'   \item{TRUE (default)}{Make the compound names pretty in column titles and
+#'   in any Word output files. This was designed for simulations where the
+#'   substrate and any metabolites, perpetrators, or perpetrator metabolites
+#'   are among the standard options for the simulator, and leaving
+#'   \code{prettify_compound_names = TRUE} will make the name of those compounds
+#'   something more human readable. For example, "SV-Rifampicin-MD" will become
+#'   "rifampicin", and "Sim-Midazolam" will become "midazolam".}
+#'
+#'   \item{FALSE}{Don't change any compound names.}
+#'
+#'   \item{a named character vector of compound IDs and what each should be
+#'   prettified to}{For example, \code{prettify_compound_names = c("perpetrator" =
 #'   "teeswiftavir", "substrate" = "superstatin")}. Please note that "perpetrator"
-#'   includes \emph{all} the perpetrators and perpetrator metabolites present, so, if
-#'   you're setting the perpetrator name, you really should use something like this
-#'   if you're including perpetrator metabolites: \code{prettify_compound_names =
-#'   c("perpetrator" = "teeswiftavir and 1-OH-teeswiftavir", "substrate" =
-#'   "superstatin")}.
+#'   includes \emph{any} of inhibitor 1, inhibitor 2, and inhibitor 1 metabolite,
+#'   so, if you're setting the perpetrator name, you really should use something
+#'   like this if you're including perpetrator metabolites:
+#'   \code{prettify_compound_names = c("perpetrator" = "teeswiftavir and 1-OH-teeswiftavir",
+#'   "substrate" = "superstatin")}.}
+#'   }
+#' @param pretty_or_ugly_cols Do you want "pretty" column names such as "Dose 1
+#'   AUCinf (ng/mL.h)" or do you want "ugly" but R friendly column names such as
+#'   "AUCinf_dose1"? Options are "pretty" (default) or "ugly".
 #'
 #' @return a PK table with prettier column names
 #' @export
@@ -31,7 +40,8 @@
 #' # None yet
 #' 
 prettify_column_names <- function(PKtable, 
-                                  prettify_compound_names = TRUE){
+                                  prettify_compound_names = TRUE, 
+                                  pretty_or_ugly_cols = "pretty"){
    
    
    # Error catching ----------------------------------------------------------
@@ -77,23 +87,41 @@ prettify_column_names <- function(PKtable,
    AllPKParameters_mod <- bind_rows(AllPKParameters_mod, 
                                     ExtraPKParam)
    
-   suppressMessages(
-      TableNames <-
-         data.frame(OrigColNames = OrigColNames, 
-                    PKparameter = names(PKtable)) %>% 
-         mutate(IsPretty = PKparameter %in% c(AllPKParameters$PrettifiedNames, 
-                                              AllPKParameters_mod$PrettifiedNames), 
-                IsNotPretty = PKparameter %in% c(AllPKParameters_mod$PKparameter, 
-                                                 AllPKParameters$PKparameter),
-                IsPKParam = IsPretty | IsNotPretty, 
-                NeedsPrettifying = IsPKParam & IsNotPretty) %>% 
-         left_join(AllPKParameters %>% 
-                      select(PKparameter, PrettifiedNames) %>% 
-                      bind_rows(ExtraPKParam)) %>% 
-         unique() %>% 
-         mutate(PrettifiedNames = ifelse(is.na(PrettifiedNames), 
-                                         PKparameter, PrettifiedNames))
-   )
+   TableNames <-
+      data.frame(OrigColNames = OrigColNames, 
+                 PKparameter_orig = names(PKtable), 
+                 OrigOrder = 1:ncol(PKtable)) %>% 
+      mutate(IsPretty = PKparameter_orig %in% c(AllPKParameters$PrettifiedNames, 
+                                                AllPKParameters_mod$PrettifiedNames), 
+             IsNotPretty = PKparameter_orig %in% c(AllPKParameters_mod$PKparameter, 
+                                                   AllPKParameters$PKparameter),
+             IsPKParam = IsPretty | IsNotPretty, 
+             NeedsPrettifying = IsPKParam & IsNotPretty, 
+             PKparameter = case_when(NeedsPrettifying == TRUE ~ PKparameter_orig, 
+                                     NeedsPrettifying == FALSE & IsPKParam == FALSE ~ PKparameter_orig), 
+             PrettifiedNames = case_when(NeedsPrettifying == FALSE ~ PKparameter_orig, 
+                                         NeedsPrettifying == FALSE & IsPKParam == FALSE ~ PKparameter_orig))
+   # Some columns may need prettifying and others may need uglifying. Need to figure
+   # out what values to fill in for any NA values in either PrettifiedNames or
+   # in PKparameter, so splitting table here.
+   
+   TableNamesToPrettify <- TableNames %>% filter(is.na(PrettifiedNames)) %>% 
+      select(-PrettifiedNames) %>% 
+      left_join(AllPKParameters %>% 
+                   select(PKparameter, PrettifiedNames) %>% 
+                   bind_rows(ExtraPKParam), 
+                by = "PKparameter") %>% unique()
+   
+   TableNamesToUglify <- TableNames %>% filter(is.na(PKparameter)) %>% 
+      select(-PKparameter) %>% 
+      left_join(AllPKParameters %>% 
+                   select(PKparameter, PrettifiedNames) %>% 
+                   bind_rows(ExtraPKParam), 
+                by = "PrettifiedNames") %>% unique()
+   
+   TableNames <- TableNames %>% filter(IsPKParam == FALSE) %>% 
+      bind_rows(TableNamesToPrettify, TableNamesToUglify) %>% 
+      arrange(OrigOrder)
    
    if(any(str_detect(OrigColNames, "NumeratorSim|DenominatorSim"))){
       Suffix <- str_extract(OrigColNames, "NumeratorSim|DenominatorSim")
@@ -102,13 +130,18 @@ prettify_column_names <- function(PKtable,
       TableNames$PrettifiedNames <- str_trim(paste(TableNames$PrettifiedNames, Suffix))
    }
    
+   # Setting semi-finalized column names. 
+   TableNames <- TableNames %>% 
+      mutate(FinalNames = case_when({pretty_or_ugly_cols} == "pretty" ~ PrettifiedNames, 
+                                    {pretty_or_ugly_cols} == "ugly" ~ PKparameter))
+   
    # Check that all column names would be unique.
-   if(any(duplicated(TableNames$PrettifiedNames))){
+   if(any(duplicated(TableNames$FinalNames))){
       warning("Some table names would be duplicated after prettification. Please check the output. For now, we're adding a number to the end of each replicate column name.\n", 
               call. = FALSE)
-      TableNames$PrettifiedNames[duplicated(TableNames$PrettifiedNames)] <- 
-         paste(TableNames$PrettifiedNames[duplicated(TableNames$PrettifiedNames)], 
-               1:length(TableNames$PrettifiedNames[duplicated(TableNames$PrettifiedNames)]))
+      TableNames$FinalNames[duplicated(TableNames$FinalNames)] <- 
+         paste(TableNames$FinalNames[duplicated(TableNames$FinalNames)], 
+               1:length(TableNames$FinalNames[duplicated(TableNames$FinalNames)]))
    }
    
    # if(complete.cases(adjust_conc_units)){
@@ -152,7 +185,7 @@ prettify_column_names <- function(PKtable,
    #    PrettyCol <- sub("perpetrator", MyPerpetrator, PrettyCol)
    # }
    
-   names(PKtable) <- TableNames$PrettifiedNames
+   names(PKtable) <- TableNames$FinalNames
    
    # } else if(complete.cases(sheet_PKparameters) & 
    #           any(str_detect(names(PKtable_all[[1]]), "_dose1|_last")) == FALSE){
