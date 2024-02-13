3a869ac1d052fbbcab23db35d406b942762f644c
Laura Shireman <laura.shireman@certara.com>
2024-02-12 21:07:57 -0800

    check_doseint - adjusted to show actual last dose rather than 0

--- R/check_doseint.R
+++ R/check_doseint.R
@@ -91,8 +91,18 @@ check_doseint <- function(sim_data_file,
              # administered OR there should be no time left.
              OneDoseIntRemaining = IntervalRemaining == DoseInt_X |
                 IntervalRemaining == 0, 
-             CompoundID = compoundID) %>% 
+             CompoundID = compoundID, 
+             EndsOnLastDose = IntervalRemaining == 0 & OneDoseIntRemaining == TRUE, 
+             LastDoseTime = ifelse(EndsOnLastDose, 
+                                   StartHr_X + (NumDoses_X - 1) * DoseInt_X, 
+                                   LastDoseTime), 
+             NumDoses_X = ifelse(EndsOnLastDose, 
+                                 NumDoses_X - 1, NumDoses_X), 
+             IntervalRemaining = ifelse(EndsOnLastDose, 
+                                        DoseInt_X, IntervalRemaining)) %>% 
+      select(-EndsOnLastDose) %>% 
       select(File, CompoundID, everything())
+
    
    # DoseInt_x will be NA when it's a single-dose simulation, in which case,
    # the dosing interval is fine.
