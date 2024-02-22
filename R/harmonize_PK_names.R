c702d9a379ae43f76cca754d19c06df4c6bde35c
Laura Shireman <laura.shireman@certara.com>
2024-02-22 14:09:08 -0800

    harmonize_PK_names: Debugged to do regex substitutions in the optimal order

--- R/harmonize_PK_names.R
+++ R/harmonize_PK_names.R
@@ -16,6 +16,19 @@ harmonize_PK_names <- function(PKparameters){
    PKparameters <- sub("_ss", "_last", PKparameters)
    PKparameters <- sub("_first", "_dose1", PKparameters)
    
+   # other misc errors
+   PKparameters <- sub("AUCt_last", "AUCtau_last", PKparameters)
+   PKparameters <- sub("AUC_inf", "AUCinf", PKparameters)
+   PKparameters <- sub("AUC_t", "AUCt", PKparameters)
+   PKparameters <- sub("AUCtau_dose1", "AUCt_dose1", PKparameters)
+   PKparameters <- sub("AUCt_ratio_last", "AUCtau_ratio_last", PKparameters)
+   PKparameters <- sub("_[iI]nhib", "_withInhib", PKparameters)
+   PKparameters <- sub("withinhib", "withInhib", PKparameters)
+   
+   # If the user switched the order of "ratio" and "last" or "dose1", fix that.
+   PKparameters <- sub("dose1_ratio", "ratio_dose1", PKparameters)
+   PKparameters <- sub("last_ratio", "ratio_last", PKparameters)
+   
    # Dealing with possible case issues
    PKparameters <- data.frame(Orig = tolower(PKparameters)) %>% 
       left_join(
@@ -27,22 +40,13 @@ harmonize_PK_names <- function(PKparameters){
                                   Rev, Orig)) %>% 
       pull(NoCaseProbs)
    
-   # If the user switched the order of "ratio" and "last" or "dose1", fix that.
-   PKparameters <- sub("dose1_ratio", "ratio_dose1", PKparameters)
-   PKparameters <- sub("last_ratio", "ratio_last", PKparameters)
-   
-   # other miscellaneous errors
+   # other miscellaneous errors for parameters that aren't the main ones
    PKparameters <- sub("cl(_)?hep", "CL_hep", PKparameters)
    PKparameters <- sub("cl(_)?po", "CL_po", PKparameters)
-   PKparameters <- sub("withinhib", "withInhib", PKparameters)
    PKparameters <- sub("um1_", "umI_", PKparameters)
    PKparameters <- sub("um2_", "umII_", PKparameters)
    PKparameters <- sub("um3_", "umIII_", PKparameters)
    PKparameters <- sub("um4_", "umIV_", PKparameters)
-   PKparameters <- sub("AUCt_last", "AUCtau_last", PKparameters)
-   PKparameters <- sub("AUCtau_dose1", "AUCt_dose1", PKparameters)
-   PKparameters <- sub("AUCt_ratio_last", "AUCtau_ratio_last", PKparameters)
-   PKparameters <- sub("_[iI]nhib", "_withInhib", PKparameters)
    PKparameters <- sub("auc tab", "AUC tab", PKparameters)
    PKparameters <- sub("absorption tab", "Absorption tab", PKparameters)
    
