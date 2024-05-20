91dffb0ce744fc432495fbdfa54f410262c39fd1
Laura Shireman <laura.shireman@certara.com>
2024-05-20 14:20:01 -0700

    list_interactions: initial commit

---
+++ R/list_interactions.R
@@ -0,0 +1,93 @@
+#' Find all the possible drug-drug interactions between compounds included in a
+#' single simulation
+#'
+#' \code{list_interactions} looks through the parameters for a Simcyp simulation
+#' for any pathways involved in the elimination of the substrate, primary
+#' metabolite 1, primary metabolite 2, or secondary metabolite that are also
+#' affected by any interactions for the inhibitor 1, inhibitor 1 metabolite, or
+#' inhibitor 2. This returns a list of a) "affected_pathways" -- the affected
+#' metabolic pathways -- and b) "interactions" -- the specific set of
+#' elimination and interaction parameters and their values in the specified
+#' simulation.
+#'
+#' @param sim_data_file name of the Excel file containing the simulated
+#'   concentration-time data, in quotes; must be an output file from the Simcyp
+#'   simulator
+#' @param existing_exp_details the output from running
+#'   \code{\link{extractExpDetails_mult}} or \code{\link{extractExpDetails}}
+#' @param include_auto_DDI TRUE or FALSE (default) for whether to list
+#'   auto-induction or auto-inhibition.
+#'
+#' @return a list: "affected_pathways" (a character vector of the metabolic
+#'   pathways affected) and "interactions" (a data.frame of all the affected
+#'   pathways and their exact values for elimination and interaction paraemters)
+#' @export
+#'
+#' @examples
+#' # none yet
+#' 
+list_interactions <- function(sim_data_file, 
+                              existing_exp_details, 
+                              include_auto_DDI = FALSE){
+   
+   # Error catching ----------------------------------------------------------
+   
+   # Check whether tidyverse is loaded
+   if("package:tidyverse" %in% search() == FALSE){
+      stop("The SimcypConsultancy R package also requires the package tidyverse to be loaded, and it doesn't appear to be loaded yet. Please run `library(tidyverse)` and then try again.")
+   }
+   
+   # Main body of function ---------------------------------------------------
+   
+   details_subset <- annotateDetails(existing_exp_details, 
+                                     sims_to_include = sim_data_file, 
+                                     simulator_section = c("elimination", 
+                                                           "interaction")) %>% 
+      mutate(Pathway = str_extract(Detail, "(CYP|UGT)[0-9]{1,2}[A-Z][0-9]{1,3}|BCRP|Pgp|OAT[237]|OCT(N)?[123]|MATE[12](K)?|OATP[124][BC][13]|MRP[2346]|ASBT|PEPT1|MCT1|ENT[12]|NTCP|MDR1|GLUT1")) %>% 
+      rename(Value = {{sim_data_file}}) %>% 
+      filter(complete.cases(Pathway) & Value != "0")
+   
+   if(include_auto_DDI){
+      
+      vic_elim <- details_subset %>% 
+         filter(SimulatorSection == "Elimination")
+      
+      perp_DDI <- details_subset %>% 
+         filter(SimulatorSection == "Interaction")
+      
+   } else {
+      
+      vic_elim <- details_subset %>% 
+         filter(CompoundID %in% AllCompounds$CompoundID[
+            AllCompounds$DosedCompoundID == "substrate"] &
+               SimulatorSection == "Elimination")
+      
+      perp_DDI <- details_subset %>% 
+         filter(CompoundID %in% AllCompounds$CompoundID[
+            AllCompounds$DosedCompoundID != "substrate"] &
+               SimulatorSection == "Interaction")
+   }
+   
+   vic_elim <- vic_elim %>% 
+      select(CompoundID, Pathway, Detail, Value)
+   
+   perp_DDI <- perp_DDI %>% 
+      select(CompoundID, Pathway, Detail, Value)
+   
+   affected_pathways <- intersect(unique(vic_elim$Pathway), 
+                                  unique(perp_DDI$Pathway))
+   
+   interactions <- details_subset %>% 
+      filter(Detail %in% c(vic_elim$Detail[vic_elim$Pathway %in% affected_pathways], 
+                           perp_DDI$Detail[perp_DDI$Pathway %in% affected_pathways])) %>% 
+      select(Pathway, CompoundID, Compound, SimulatorSection, Detail, Value, Notes) %>% 
+      arrange(Pathway)
+   
+   return(list("affected_pathways" = affected_pathways, 
+               "interactions" = interactions))
+   
+}
+
+
+
+
