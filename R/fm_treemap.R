203b3ad61cf67923c14fd2367762e6524df19b47
Laura Shireman <laura.shireman@certara.com>
2024-07-17 11:10:10 -0700

    fm_treemap: Added some options for dealing w/DDIs and for when there's more than one tissue for a given pathway

--- R/fm_treemap.R
+++ R/fm_treemap.R
@@ -20,6 +20,12 @@
 #'   data you want. For example, if this was a DDI simulation, you would want to
 #'   use only the data in the absense of a perpetrator or only in the presence
 #'   of one; summing up both numbers wouldn't make sense.
+#' @param DDI_option If the fm_dataframe included fm and fe values for when any
+#'   DDI perpetrator drugs are present, then you've got some options for how to
+#'   display this. Acceptable options are: "baseline only" (default) to show
+#'   only fm values at baseline, "DDI only", to show only fm values in the
+#'   presence of the perpetrator drug(s), or "facet by DDI" to show two graphs,
+#'   broken up by whether there is a DDI.
 #' @param pathway_column the name of the column that contains the names of the
 #'   pathways. If you ran \code{\link{extractFmFe}} to get these data, set this
 #'   to \code{pathway_column = Enzyme}
@@ -30,6 +36,8 @@
 #'   occupied by boxes in your treemap will sum to 100\%, even if these numbers
 #'   do not, so pay attention to what data you're graphing and make sure things
 #'   sum to 1.
+#' @param show_numbers_on_graph TRUE (default) or FALSE for whether to list the
+#'   numerical fm value for each pathway on the graph
 #' @param rounding option for what rounding to perform, if any. Options are:
 #'   \describe{
 #'
@@ -86,6 +94,8 @@
 fm_treemap <- function(fm_dataframe,
                        pathway_column, 
                        fm_column, 
+                       DDI_option = "baseline only", 
+                       show_numbers_on_graph = TRUE, 
                        rounding = NA, 
                        color_set = "default", 
                        label_fm_cutoff = 0.05,
@@ -121,110 +131,179 @@ fm_treemap <- function(fm_dataframe,
    pathway_column <- rlang::enquo(pathway_column)
    
    if(rlang::as_label(fm_column) %in% names(fm_dataframe) == FALSE){
-      stop("The column you have listed for the fm values is not present in your data.frame. Please enter a valid column for fm data.",
+      stop(wrapn("The column you have listed for the fm values is not present in your data.frame. Please enter a valid column for fm data."),
            call. = FALSE)
    }
    
    if(rlang::as_label(pathway_column) %in% names(fm_dataframe) == FALSE){
-      stop("The column you have listed for the pathways is not present in your data.frame. Please enter a valid column for pathway data.",
+      stop(wrapn("The column you have listed for the pathways is not present in your data.frame. Please enter a valid column for pathway data."),
            call. = FALSE)
    }
    
    if(biggest_box_position %in% c("bottom left", "top left", "bottom right", 
                                   "top right") == FALSE){
-      warning(paste0("The only options for `biggest_box_position` are `bottom left`, `top left`, `bottom right`, or `top right`, and you supplied a value of `", 
-                     biggest_box_position,
-                     "``, which is not one of the options. We'll use the default of `bottom left`."), 
+      warning(wrapn(paste0("The only options for `biggest_box_position` are `bottom left`, `top left`, `bottom right`, or `top right`, and you supplied a value of `", 
+                           biggest_box_position,
+                           "``, which is not one of the options. We'll use the default of `bottom left`.")), 
               call. = FALSE)
       biggest_box_position <- "top left"
    }
    
    rounding <- ifelse(is.na(rounding), "significant 3", rounding)
    
+   if(DDI_option %in% c("baseline only", "DDI only", "facet by DDI") == FALSE){
+      warning(wrapn("The value you supplied for DDI_option is not one of `baseline only`, `DDI only`, or `facet by DDI`, which are the only acceptable options. We'll set this to the default of `baseline only`."), 
+              call. = FALSE)
+      DDI_option <- "baseline only"
+   }
+   
    # Main function ----------------------------------------------------------
    
    # Adding labels 
    fm_dataframe <- fm_dataframe %>% 
-      rename(DME = !! pathway_column,
-             fm = !! fm_column) 
+      rename(DME = !!pathway_column,
+             fm = !!fm_column)
    
-   # Rounding as requested
-   fm_dataframe <- fm_dataframe %>%
-      mutate(fm = round_opt(fm*100, round_fun = rounding, is_this_for_Word = FALSE)) %>% 
-      # adding labels
-      mutate(Label = paste0(DME, "\n", fm, "%"), 
-             LabelLegend = paste0(DME, " ", fm*100, "%"), 
+   if(DDI_option == "baseline only"){
+      fm_dataframe <- fm_dataframe %>% filter(PerpPresent == FALSE)
+   } else if(DDI_option == "DDI only"){
+      fm_dataframe <- fm_dataframe %>% filter(PerpPresent == TRUE)
+   }
+   
+   # Making a prettier label for any faceting by DDI
+   fm_dataframe <- fm_dataframe %>% 
+      mutate(Perpetrator = ifelse(PerpPresent, "DDI", "baseline"), 
+             Perpetrator = factor(Perpetrator, levels = c("baseline", "DDI")))
+   
+   # If any of the DME are replicated in multiple tissues, we'll need to note
+   # that in the labels. I don't think we want labels that say, e.g., "liver
+   # CYP3A4", "liver CYP1A2", etc. when they're ALL liver, though.
+   suppressMessages(
+      TissueDMECheck <- fm_dataframe %>% 
+         group_by(DME, PerpPresent) %>% 
+         summarize(N = n()) %>% 
+         filter(N > 1)
+   )
+   
+   if(nrow(TissueDMECheck) > 0){
+      fm_dataframe <- fm_dataframe %>% 
+         mutate(DME = case_when(DME %in% TissueDMECheck$DME ~ paste(Tissue, DME), 
+                                .default = DME))
+   }
+   
+   fm_dataframe <- fm_dataframe %>% 
+      # Rounding as requested
+      mutate(fm = round_opt(fm*100, round_fun = rounding, is_this_for_Word = FALSE), 
+             # Dealing with possible renal elimination
+             DME = case_when(is.na(DME) & Parameter == "fe" ~
+                                paste(Tissue, "elimination"), 
+                             .default = DME), 
+             # adding labels
+             Label = switch(as.character(show_numbers_on_graph), 
+                            "TRUE" = paste0(DME, "\n", fm, "%"), 
+                            "FALSE" = DME), 
+             LabelLegend = paste0(DME, " ", fm, "%"), 
              Label = fct_reorder(Label, fm, .desc = TRUE), 
              LabelLegend = fct_reorder(LabelLegend, fm, .desc = TRUE)) %>% 
       arrange(Label)
    
    
-   # Adding options for colors
-   NumColors <- length(levels(fm_dataframe$Label))
-   
-   if(length(color_set) > 1){
+   # subfunction so that we can create multiple sets of graphs as needed
+   tree_subfun <- function(DDI){
+      fm_dataframe <- fm_dataframe %>% filter(PerpPresent == DDI)
       
-      MyColors <- color_set
+      # Adding options for colors
+      NumColors <- length(unique(fm_dataframe$Label))
       
-      if(length(MyColors) != NumColors){
-         warning(paste("You supplied", length(color_set), "colors, but we need", 
-                       NumColors, "colors. We will have to use the default color set instead.\n"), 
-                 call. = FALSE)
+      if(length(color_set) > 1){
          
-         color_set <- "default"
+         MyColors <- color_set
+         
+         if(length(MyColors) != NumColors){
+            warning(paste("You supplied", length(color_set), "colors, but we need", 
+                          NumColors, "colors. We will have to use the default color set instead.\n"), 
+                    call. = FALSE)
+            
+            color_set <- "default"
+         }
+      } 
+      
+      if(length(color_set) == 1){
+         
+         if(color_set == "default" & NumColors > 5){
+            color_set <- "viridis"
+            warning("You requested the color_set 'default', which has 5 possible colors, but your graph requires more colors than that. The color set 'viridis' will be used instead.", 
+                    call. = FALSE)
+         }
+         
+         MyColors <- switch(color_set, 
+                            "default" = SimcypColors, 
+                            "set 1" = RColorBrewer::brewer.pal(NumColors, "Set1"),
+                            "set 2" = RColorBrewer::brewer.pal(NumColors, "Dark2"),
+                            "rainbow" = rainbow(NumColors), 
+                            "blue-green" = blueGreens(NumColors),
+                            "blues" = blues(NumColors),
+                            "viridis" = viridis::viridis(NumColors))
+         
+         # Adjusting default colors based on number of levels to the combos I like
+         # best. :-)
+         if(color_set == "default" & NumColors > 1 & NumColors < 5){
+            MyColors <- switch(as.character(NumColors), 
+                               "2" = SimcypColors[2:3], 
+                               "3" = SimcypColors[2:4], 
+                               "4" = SimcypColors[2:5])
+         }
+         
       }
-   } 
-   
-   if(length(color_set) == 1){
       
-      if(color_set == "default" & NumColors > 5){
-         color_set <- "viridis"
-         warning("You requested the color_set 'default', which has 5 possible colors, but your graph requires more colors than that. The color set 'viridis' will be used instead.", 
-                 call. = FALSE)
+      names(MyColors) <- unique(fm_dataframe$LabelLegend)
+      
+      # Putting into the legend any fm's that are below the threshold
+      G <- ggplot(fm_dataframe, aes(label = Label, area = fm, fill = LabelLegend)) +
+         treemapify::geom_treemap(start = sub(" ", "", biggest_box_position)) +
+         treemapify::geom_treemap_text(fontface = "bold", colour = "white", place = "centre", 
+                                       min.size = 6, 
+                                       start = sub(" ", "", biggest_box_position)) +
+         scale_fill_manual(
+            # FIXME - Fiddle with this to get only the legend entries that are not
+            # already labeled in hte graph
+            # breaks = fm_dataframe$LabelLegend[which(fm_dataframe$fm < label_fm_cutoff)],
+            values = MyColors) +
+         theme(legend.title = element_blank(),
+               legend.position = "bottom")
+      
+      if(complete.cases(legend_nrow)){
+         G <- G + guides(fill = guide_legend(nrow = legend_nrow))
       }
       
-      MyColors <- switch(color_set, 
-                         "default" = SimcypColors, 
-                         "set 1" = RColorBrewer::brewer.pal(NumColors, "Set1"),
-                         "set 2" = RColorBrewer::brewer.pal(NumColors, "Dark2"),
-                         "rainbow" = rainbow(NumColors), 
-                         "blue-green" = blueGreens(NumColors),
-                         "blues" = blues(NumColors),
-                         "viridis" = viridis::viridis(NumColors))
-      
-      # Adjusting default colors based on number of levels to the combos I like
-      # best. :-)
-      if(color_set == "default" & NumColors > 1 & NumColors < 5){
-         MyColors <- switch(as.character(NumColors), 
-                            "2" = SimcypColors[2:3], 
-                            "3" = SimcypColors[2:4], 
-                            "4" = SimcypColors[2:5])
+      if(DDI_option == "facet by DDI"){
+         G <- G + ggtitle(ifelse(DDI, "DDI", "baseline")) +
+            theme(plot.title = element_text(hjust = 0.5))
       }
       
+      return(G)
    }
    
-   names(MyColors) <- levels(fm_dataframe$LabelLegend)
-   
-   # Putting into the legend any fm's that are below the threshold
-   G <- ggplot(fm_dataframe, aes(label = Label, area = fm, fill = LabelLegend)) +
-      treemapify::geom_treemap(start = sub(" ", "", biggest_box_position)) +
-      treemapify::geom_treemap_text(fontface = "bold", colour = "white", place = "centre", 
-                                    min.size = 6, 
-                                    start = sub(" ", "", biggest_box_position)) +
-      scale_fill_manual(
-         breaks = fm_dataframe$LabelLegend[which(fm_dataframe$fm < label_fm_cutoff)],
-         values = MyColors) +
-      theme(legend.title = element_blank(),
-            legend.position = "bottom")
-   
-   if(complete.cases(legend_nrow)){
-      G <- G + guides(fill = guide_legend(nrow = legend_nrow))
-   }
-   
-   if(("character" %in% class(graph_title) && complete.cases(graph_title)) |
-      "expression" %in% class(graph_title)){
-      G <- G + ggtitle(graph_title) +
-         theme(plot.title = element_text(hjust = 0.5, size = graph_title_size))
+   if(DDI_option == "facet by DDI"){
+      A <- tree_subfun(FALSE)
+      B <- tree_subfun(TRUE)
+      G <- ggpubr::ggarrange(A, B, nrow = 1, common.legend = FALSE, align = "hv")
+      
+      if(("character" %in% class(graph_title) && complete.cases(graph_title)) |
+         "expression" %in% class(graph_title)){
+         G <- ggpubr::annotate_figure(G, 
+                                      top = ggpubr::text_grob(graph_title, 
+                                                              size = graph_title_size))
+      }
+   } else {
+      
+      G <- tree_subfun(DDI = DDI_option == "DDI only") 
+      
+      if(("character" %in% class(graph_title) && complete.cases(graph_title)) |
+         "expression" %in% class(graph_title)){
+         G <- G + ggtitle(graph_title) +
+            theme(plot.title = element_text(hjust = 0.5, size = graph_title_size))
+      }
    }
    
    if(complete.cases(save_graph)){
