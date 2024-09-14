2a0535c7ce6d64c9e3175b2e7d8d899d85b172e4
Laura Shireman <laura.shireman@certara.com>
2024-09-13 18:44:48 -0700

    inductFit: Fixed a few bugs that were omitting some values in the output or not making them bold face.

--- R/extractConcTime_mult.R
+++ R/extractConcTime_mult.R
@@ -293,7 +293,11 @@ extractConcTime_mult <- function(sim_data_files = NA,
    # etc.
    
    # If user supplied an unquoted object, this checks whether that object
-   # exists. However, if they supplied NA or NULL, this throws an error. 
+   # exists. However, if they supplied NA or NULL, this throws an error. Also,
+   # "exists" does not work with operators, so, if the user supplied an item in
+   # a list, e.g., they split ct_dataframe by whether it was a development or
+   # verification sim or something, supplying, e.g., CT[[i]] will result in
+   # FALSE here.
    Recode_ct_dataframe <- suppressWarnings(
       try(exists(deparse(substitute(ct_dataframe))) == FALSE, silent = TRUE))
    
