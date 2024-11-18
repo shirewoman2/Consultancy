b2ff8ff0e67081784d03e1c6379bf3e1648930f8
Laura Shireman <laura.shireman@certara.com>
2024-11-18 14:02:06 -0800

    I think this should be the last step to getting this back to regular beta? Revert "extractPK and renameStats: Working toward harmonizing the way package refers to stats b/c it's a bit of a mess."
    This reverts commit f7bb60b6a6c4252117d9b37681556e99b377a45e.

--- R/renameStats.R
+++ R/renameStats.R
@@ -2,96 +2,50 @@
 #'
 #' \code{renameStats} takes Simulator statistic names such as "90% confidence
 #' interval around the geometric mean(lower limit)" and converts them to names
-#' that would work for R column names, e.g., "CI90_lower", or converts them to
-#' names that comply with Simcyp Consultancy Team report templates. This is
-#' mainly meant for internal use in the SimcypConsultancy package to help make
-#' sure we're naming things consistently.
+#' that would work for R column names, e.g., "CI90_low". This is mainly meant
+#' for internal use in the SimcypConsultancy package to help make sure we're
+#' naming things consistently.
 #'
 #' @param OrigStat the original statistic name
-#' @param use options are "internal" for internal use when renaming stats from
-#'   Simulator-output names into R-friendly column names or "report" for
-#'   renaming R columns into the values that show up in the Simcyp Consultancy
-#'   report template tables of PK parameters.
 #'
 #' @return an R-friendly version of that statistic name
 #' @export
 #'
-#' @examples
+#' @examples#' 
 #' renameStats(c("Geometric Mean", "Min Val", "5th centile"))
 #' 
-renameStats <- function(OrigStat, 
-                        use = "internal"){
-   
-   # NB: As of Nov. 2024, making the names be title case to avoid confusion with
-   # the base R functions. Note that Geomean is title case and NOT camel case
-   # and that minimum and maximum are spelled out.
-   AllStats <- tibble(
-      InternalColNames = c("Mean", "Mean", "Geomean", "Geomean", 
-                           "Median", "Median", "GCV", "GCV", 
-                           "CI90_lower", "CI90_upper", NA, # concatenated 90% CI
-                           "CI95_lower", "CI95_upper", NA, # concatenated 95% CI
-                           "Per5", "Per95", NA, # concatenated percentiles
-                           "Skewness", "Skewness", 
-                           "CV", "Minimum", "Minimum", "Maximum", "Maximum", NA, # range
-                           "Fold", "Fold", "SD", "SD", 
-                           "MinMean", "MinMean", "MaxMean", "MaxMean", # used in pk_table function internally 
-                           "TrialMeanRange", 
-                           # obs values
-                           rep(NA, 10)), 
-      
-      SimulatorNames = c("Mean", "mean", "Geometric Mean", "geometric mean", 
-                         "Median", "median", "Geometric CV", "geometric cv",
-                         "90% confidence interval around the geometric mean(lower limit)", 
-                         "90% confidence interval around the geometric mean(upper limit)", 
-                         NA, # concatenated 90% CI
-                         "95% confidence interval around the geometric mean(lower limit)", 
-                         "95% confidence interval around the geometric mean(upper limit)", 
-                         NA, # concatenated 95% CI
-                         "5th centile", "95th centile", NA, # concatenated percentiles
-                         "Skewness", "skewness",
-                         "cv", "Min Val", "min val", "Max Val", "max val", NA, # range
-                         "Fold", "fold", "Std Dev", "std dev", 
-                         "MinMean", "minMean", "maxmean", "MaxMean", # used in pk_table function internally
-                         "TrialMeanRange", 
-                         # obs values
-                         rep(NA, 10)), 
-      
-      ReportNames = c(rep("Simulated", 6), # mean through median
-                      "CV%", "CV%", 
-                      "90% CI - Lower", "90% CI - Upper", 
-                      "90% CI", 
-                      "95% CI - Lower", "95% CI - Upper", 
-                      "95% CI", 
-                      "5th Percentile", "95th Percentile", 
-                      "5th to 95th Percentile", 
-                      NA, NA, # params we don't report
-                      "CV%", # this is the arithmetic CV
-                      "Minimum", "Minimum", "Maximum", "Maximum", "Range", 
-                      NA, NA, # params we don't report
-                      "Standard deviation", "Standard deviation",  
-                      NA, NA, NA, NA, "Range of trial means", 
-                      
-                      # obs PK 
-                      "Observed", "Observed", "Observed CV%", "Observed CV%", 
-                      "observed CI - Lower", "observed CI - Upper",
-                      "Observed CI", "Observed range", "S/O", 
-                      "S/O range for trial means"))
-   
-   if(use == "internal"){
-      # This is when we're starting from Simulator names and going to R column
-      # names
-      AllStats <- AllStats %>% filter(SimulatorNames %in% OrigStat)
-      StatNames <- AllStats$InternalColNames
-      names(StatNames) <- AllStats$SimulatorNames
-   } else {
-      # This is when we're starting from R column names and going to report
-      # names
-      AllStats <- AllStats %>% filter(InternalColNames %in% OrigStat)
-      StatNames <- AllStats$ReportNames
-      names(StatNames) <- AllStats$InternalColNames
-   }
-   
-   return(StatNames[OrigStat])
-   
+renameStats <- function(OrigStat){
+    StatNames <- c("Mean" = "mean", 
+                   "mean" = "mean", 
+                   "Geometric Mean" = "geomean",
+                   "geometric mean" = "geomean",
+                   "Median" = "median",
+                   "median" = "median",
+                   "Geometric CV" = "GCV",
+                   "geometric cv" = "GCV",
+                   "90% confidence interval around the geometric mean(lower limit)" = "CI90_low",
+                   "90% confidence interval around the geometric mean(upper limit)" = "CI90_high",
+                   "95% confidence interval around the geometric mean(lower limit)" = "CI95_low",
+                   "95% confidence interval around the geometric mean(upper limit)" = "CI95_high",
+                   "5th centile" = "per5",
+                   "95th centile" = "per95",
+                   "Skewness" = "skewness", 
+                   "skewness" = "skewness", 
+                   "cv" = "CV",
+                   "Min Val" = "min",
+                   "min val" = "min",
+                   "Max Val" = "max",
+                   "max val" = "max",
+                   "Fold" = "fold",
+                   "fold" = "fold",
+                   "Std Dev" = "SD", 
+                   "std dev" = "SD", 
+                   # These next ones are only for inside the PKsummary_table
+                   # function and apply to trial-mean min and trial-mean max
+                   "MinMean" = "MinMean", 
+                   "minmMean" = "MinMean", 
+                   "maxmean" = "MaxMean", 
+                   "MaxMean" = "MaxMean")
+    
+    StatNames[OrigStat]
 }
-
