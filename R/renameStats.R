77b5821f23fc4e53f7da6218e8e45f310ebc8df3
Laura Shireman <laura.shireman@certara.com>
2024-12-03 16:53:36 -0800

    merging from database branch

--- R/renameStats.R
+++ R/renameStats.R
@@ -3,29 +3,36 @@
 #'
 #' \code{renameStats} takes Simulator statistic names such as "90% confidence
 #' interval around the geometric mean(lower limit)" and converts them to names
-#' that would work for R column names, e.g., "CI90_low". This is mainly meant
-#' for internal use in the SimcypConsultancy package to help make sure we're
-#' naming things consistently.
+#' that would work for R column names, e.g., "CI90_lower", or converts them to
+#' names that comply with Simcyp Consultancy Team report templates. This is
+#' mainly meant for internal use in the SimcypConsultancy package to help make
+#' sure we're naming things consistently.
 #'
 #' @param OrigStat the original statistic name
 #' @param use options are "Simulator to R" for internal use when renaming stats
 #'   from Simulator-output names into R-friendly column names, "report" for
+#'   renaming R columns into the values that show up in the Simcyp Consultancy
 #'   report template tables of PK parameters, or "report to R" when reversing
 #'   from report-friendly names to R-friendly names.
 #'
 #' @return an R-friendly version of that statistic name
 #'
-#' @examples#' 
+#' @examples
 #' renameStats(c("Geometric Mean", "Min Val", "5th centile"))
 #' 
-renameStats <- function(OrigStat){
+renameStats <- function(OrigStat, 
                         use = "Simulator to R"){
+   
    use <- tolower(use)
    if(use == "simulator to r"){
+      # This is when we're starting from Simulator names and going to R column
+      # names
       MyStats <- AllStats %>% filter(SimulatorNames %in% OrigStat)
       StatNames <- MyStats$InternalColNames
       names(StatNames) <- MyStats$SimulatorNames
    } else if(use == "report"){
+      # This is when we're starting from R column names and going to report
+      # names
       MyStats <- AllStats %>% filter(InternalColNames %in% OrigStat) %>% 
          select(InternalColNames, ReportNames) %>% unique()
       StatNames <- MyStats$ReportNames
@@ -35,6 +42,9 @@ renameStats <- function(OrigStat){
          select(ReportNames, InternalColNames) %>% unique()
       StatNames <- MyStats$InternalColNames
       names(StatNames) <- MyStats$ReportNames
-    
-    StatNames[OrigStat]
+   }
+   
+   return(StatNames[OrigStat])
+   
 }
+
