0c150b4af655be121d5fa64d3e9f58c26404f75f
Laura Shireman <laura.shireman@certara.com>
2025-11-06 20:49:47 -0800

    recalc_PK: Fixing bug w/reporting AIC and RSS when k is negative.

--- R/recalc_PK.R
+++ R/recalc_PK.R
@@ -892,7 +892,8 @@ recalc_PK <- function(ct_dataframe,
                            sub("simulated ", "simulated\n", j)))
          }
          
-         if("Estimates" %in% names(TEMP)){
+         if("Estimates" %in% names(TEMP) && 
+            TEMP$Estimates$Estimate[TEMP$Estimates$Beta == "k"] > 0){
             ElimFits[[j]] <- TEMP$Estimates
             ExtrapProbs <- FALSE
          } else {
